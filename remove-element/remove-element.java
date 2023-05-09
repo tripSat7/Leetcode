5a82a51e77694c9b230220de5b891c8e77d36f3d
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-05-09 23:22:25 +0530

    Time: 0 ms, Memory: 41.4 MB - LeetHub

---
+++ remove-element/remove-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int front = 0,rear = nums.length-1, counter=0;
+        while(front<=rear&&front<nums.length&&rear>=0){
+            if(nums[front]==val){
+                if(nums[rear]!=val){
+                    nums[front]=nums[front]+nums[rear];
+                    nums[rear]=nums[front]-nums[rear];
+                    nums[front]=nums[front]-nums[rear];
+                    front++;rear--;
+                }else{
+                    rear--;
+                }
+                // counter++; 
+            }
+            else{
+                front++;
+            }
+        }
+        return front;
+    }
+}
