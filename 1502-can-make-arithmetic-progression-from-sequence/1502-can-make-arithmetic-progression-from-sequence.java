6cb5b5b79cf20f63f24a725e88de0226edcfc34c
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-06 20:42:32 +0530

    Time: 1 ms (99.24%), Space: 40.8 MB (94.45%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
+        if(arr.length<=2){
+            return true;
+        }
         Arrays.sort(arr);
         int diff = arr[1]-arr[0];
         for(int i=2;i<arr.length;i++){
