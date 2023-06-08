5feec5776d512196976db88b66a86bda799092b9
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-08 13:44:11 +0530

    Time: 2 ms (100.00%), Space: 44.5 MB (21.08%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,18 +1,38 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        Map<Integer,String> map = new HashMap<Integer,String>();
-        for(int i=0;i<names.length;i++){
-            map.put(heights[i],names[i]);
-        }
-        Arrays.sort(heights);
-        String res[] = new String[names.length];
-        int idx=0;
-        for(int i=heights.length-1;i>=0;i--){
-            res[idx] = map.get(heights[i]);
-            idx++;
-        }
         
-        return res;
+        quickSort(names,heights,0,names.length-1);
+        return names; 
+    }
+    
+    public static void quickSort(String[] names, int[] heights, int l, int h){
         
+        if(h>l){
+            int p = partition(names,heights,l,h);
+            quickSort(names,heights,l,p-1);
+            quickSort(names,heights,p+1,h);
+        }
+    }
+    
+    public static int partition(String[] names,int[] heights,int l, int h){
+        int piv=l;
+        int i=piv;
+        for(int j=l+1;j<=h;j++){
+            if(heights[j]>=heights[piv]){
+                i++;
+                swap(names, heights, i, j);
+            }
+        }
+        swap(names, heights, piv, i);
+        return i;
+    }
+    
+    public static void swap(String[] names,int[] heights, int i, int j){
+        int temp = heights[i];
+        heights[i] = heights[j];
+        heights[j]= temp;
+        String temp1 = names[i];
+        names[i]=names[j];
+        names[j] = temp1;
     }
 }
