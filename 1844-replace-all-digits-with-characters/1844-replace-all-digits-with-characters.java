d0817cfe0be54c67b34c87367ce9074eb1929afb
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-08 16:07:41 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (93.68%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.java
@@ -2,9 +2,7 @@ class Solution {
     public String replaceDigits(String s) {
         char ch[] = s.toCharArray();
         for(int i=1;i<ch.length;i=i+2){
-            int n = ch[i] - '0'; 
-            ch[i] = (char)(ch[i-1] + n);
-            System.out.println(ch[i]);
+            ch[i]+=ch[i-1]-'0';
         }
         
         return new String(ch);
