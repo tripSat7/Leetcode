2a09ba295deed1d07819c42fafdea44e0e8dae54
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-10 16:09:46 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (43.91%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,21 +1,14 @@
 class Solution {
     public String generateTheString(int n) {
-        String res = "";
-        if(n%2!=0){
-            res = res + generate(n);
+        StringBuilder sb = new StringBuilder();
+
+        if(n%2==0){
+            sb.append("a".repeat(n-1));
+            sb.append("b");
         }else{
-            res = res + generate(n-1)+"b"; 
+            sb.append("a".repeat(n));
         }
-        
-        return res;
-    }
-    
-    public static String generate(int n){
-        StringBuilder res = new StringBuilder();
-        for(int i = 0;i<n;i++){
-            res.append('a');
-        }
-        
-        return res.toString();
+
+        return sb.toString();
     }
 }
