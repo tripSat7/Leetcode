2f3038a2bc4f721980457db038f48a16d5cf0203
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-11 00:52:00 +0530

    Time: 2 ms (84.34%), Space: 43.1 MB (23.92%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,17 +1,18 @@
 class Solution {
-    public String destCity(List<List<String>> paths) {
-        if(paths.size()==1){
-           return paths.get(0).get(1);
-        } 
+    public String destCity(List<List<String>> paths) { 
         Map<String,String> map = new HashMap<String,String>();
+        
         for(List<String> lists:paths){
             map.put(lists.get(0),lists.get(1));
         }
-        String start=paths.get(0).get(0),res = start;
-        while(map.containsKey(start)){
-            res=map.get(start);
-            start=res;
+        
+        for(List<String> list:paths){
+            String res = list.get(1); 
+            if (!map.containsKey(res)) {
+                return res; 
+            }
         }
-        return res;
+        
+        return "";
     }
 }
