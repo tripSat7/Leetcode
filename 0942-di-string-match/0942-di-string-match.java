b79c8d093ff361ce22c85a511469da191877b71d
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-10 17:13:07 +0530

    Time: 2 ms (97.58%), Space: 45.2 MB (25.13%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.java
+++ 0942-di-string-match/0942-di-string-match.java
@@ -2,17 +2,16 @@ class Solution {
     public int[] diStringMatch(String s) {
         int res[] = new int[s.length()+1];
         int ic=0,dc=s.length();
-        int i=0;
-        
-        for(char ch:s.toCharArray()){
-            if(ch=='I'){
+        int i;
+        char ch[]=s.toCharArray();
+        for(i=0;i<ch.length;i++){
+            if(ch[i]=='I'){
                 res[i] = ic;
                 ic++;
             }else{
                 res[i] = dc;
                 dc--;
             }
-            i++;
         }
         
         res[i]=ic;
