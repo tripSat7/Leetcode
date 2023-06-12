5c73f956a77d17d537ebd41b49ba53c4031efee5
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-12 16:41:19 +0530

    Time: 5 ms (96.42%), Space: 43.4 MB (40.87%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int hor=0,ver=0;
+        int x = 0, y = 0;
     
         for(char c : moves.toCharArray()){
-            if(c == 'U') ver++;
-            else if(c == 'D') ver--;
-            else if(c == 'L') hor--;
-            else if(c == 'R') hor++;
+            if(c == 'U') x++;
+            else if(c == 'D') x--;
+            else if(c == 'L') y--;
+            else if(c == 'R') y++;
         }
 
-        return ver ==0 && hor ==0;
+        return x ==0 && y ==0;
     }
 }
