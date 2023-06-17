81f10c4ea146b9f2da7a457aba02693d9ceebf40
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-17 19:23:51 +0530

    Time: 4 ms (95.13%), Space: 39.4 MB (98.96%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -1,12 +1,13 @@
 class Solution {
     public int sumOfMultiples(int n) {
-        int res=0;
-        
-        for(int i=3;i<=n;i++){
-            if(i%3==0||i%5==0||i%7==0){
-                res+=i;
+        int sum = 0;
+
+        for (int i = 3; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum = sum + i;
             }
         }
-        return res;
+
+        return sum;
     }
 }
