aa698d08848d05cc4fd80a16bd354e6f724134f9
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-19 00:48:57 +0530

    Time: 4 ms (79.54%), Space: 43.7 MB (68.38%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,19 +1,19 @@
 class Solution {
     public String reverseVowels(String s) {
         int i=0, j=s.length()-1;
-        List<Character> vowels = new ArrayList (Arrays.asList('a','A','e','E','i','I','o','O','u','U'));
+        // List<Character> vowels = new ArrayList (Arrays.asList('a','A','e','E','i','I','o','O','u','U'));
         StringBuilder sb = new StringBuilder(s);
         
         while(i<j){
-            if(!vowels.contains(sb.charAt(i))){
+            if(!isVowel(sb.charAt(i))){
                 i++;
             }
             
-            if(!vowels.contains(sb.charAt(j))){
+            if(!isVowel(sb.charAt(j))){
                 j--;
             }
             
-            if(vowels.contains(sb.charAt(i))&&vowels.contains(sb.charAt(j))){
+            if(isVowel(sb.charAt(i))&&isVowel(sb.charAt(j))){
                 char temp = sb.charAt(i);
                 sb.setCharAt(i,sb.charAt(j));
                 sb.setCharAt(j,temp);
@@ -22,8 +22,13 @@ class Solution {
             }
         }
         
-        
-        
         return sb.toString();
+    }
+    
+    public static boolean isVowel(char c){
+        if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O'||c=='u'||c=='U'){
+            return true;
+        }
+        return false;
     }
 }
