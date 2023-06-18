06cb4c603ad3cd0191c5b93089433315b144e3f9
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-18 10:39:24 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (9.61%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -3,8 +3,7 @@ class Solution {
         int[] res = new int[nums.length*2];
 
         for(int i = 0;i<nums.length;i++){
-            res[i] = nums[i];
-            res[i+nums.length] =nums[i];
+            res[i] = res[i+nums.length] =nums[i];
         }
         
         return res;
