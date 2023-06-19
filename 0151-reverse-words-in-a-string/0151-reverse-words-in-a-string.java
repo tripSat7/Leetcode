722d99cf6283e3e0a96afed8d88e554d90507f20
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-19 11:05:19 +0530

    Time: 5 ms (90.84%), Space: 43.7 MB (32.01%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -4,20 +4,11 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for(int i=arr.length-1;i>=0;i--){
-            if(!arr[i].equals("")){
+             if(!arr[i].equals("")){
                 sb.append(arr[i]);
                 sb.append(" ");
             }   
         }
-        
-        // int j=sb.length()-1;
-        // while(true){
-        //     if(sb.charAt(j)!=' '){
-        //         break;
-        //     }
-        //     j--;
-        // }
-        // String res=sb.substring(0,j+1);
         
         return sb.toString().trim();
     }
