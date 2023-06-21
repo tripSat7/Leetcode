d326f097c6a6d63bdbd317598f8910764fe21dea
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-22 01:44:12 +0530

    Time: 93 ms (17.60%), Space: 44.9 MB (11.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
-        int low=0;
-        int high=s.length()-1;
-        while(low<high){
-            if(s.charAt(low)!=s.charAt(high))
-            {
+        char ch[] = s.toLowerCase().toCharArray();
+        int i=0, j=ch.length-1;
+        while(i<j){
+            // System.out.println(ch[i]>='0');
+
+            while(!((ch[i]>='a' && ch[i]<='z')||(ch[i]>='0' && ch[i]<='9')) && i<j){
+                i++;
+            }
+            while(!((ch[j]>='a' && ch[j]<='z')||(ch[j]>='0' && ch[j]<='9')) && i<j){
+                j--;
+            }
+            System.out.println(ch[i]+"::"+ch[j]);
+            if(!(ch[i]==ch[j])){
                 return false;
             }
-            low++;high--;
+            i++;j--;
         }
         return true;
     }
