f6c517ac917080c12bb2785a8632005c17cddcdf
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-22 01:59:08 +0530

    Time: 93 ms (17.60%), Space: 44.8 MB (11.74%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        char ch[] = s.toLowerCase().toCharArray();
+        char ch[] = s.toCharArray();
         int i=0, j=ch.length-1;
         while(i<j){
 
-            while(!((ch[i]>='a' && ch[i]<='z')||(ch[i]>='0' && ch[i]<='9')) && i<j){
+            while(!((ch[i]>=65 && ch[i]<=90) || (ch[i]>=97 && ch[i]<=122) ||(ch[i]>='0' && ch[i]<='9')) && i<j){
                 i++;
             }
-            while(!((ch[j]>='a' && ch[j]<='z')||(ch[j]>='0' && ch[j]<='9')) && i<j){
+            while(!((ch[j]>=65 && ch[j]<=90) || (ch[j]>=97 && ch[j]<=122) ||(ch[j]>='0' && ch[j]<='9')) && i<j){
                 j--;
             }
             System.out.println(ch[i]+"::"+ch[j]);
-            if(!(ch[i]==ch[j]) || i>j){
+            if(!(Character.toLowerCase(ch[i])==Character.toLowerCase(ch[j])) || i>j){
                 return false;
             }
             i++;j--;
