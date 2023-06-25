c34e553ab11eac166675df2497ebb1899f61ad48
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-25 16:26:39 +0530

    Time: 16 ms (5.43%), Space: 41.2 MB (5.88%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        int start=0, i;
+        char hay[] = haystack.toCharArray();
+        char ned[] = needle.toCharArray();
+        for(i=0;i<hay.length;i++){
+            int t1=i;
+            //System.out.println(hay[t1]+"::"+ned[start]);
+            while(t1<hay.length &&  start<ned.length && hay[t1]==ned[start]){
+                System.out.println(hay[t1]+"=="+ned[start]);
+                t1++;start++;
+            }
+            
+            if(start == ned.length){
+                return t1-start;
+            }else{
+                start=0;
+            }
+        }
+        return -1;
+    }
+}
