9e7e0132fa5d78039dd6ddc9cb1fdcaf1f8a8e4e
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-28 02:31:18 +0530

    Time: 1 ms (92.95%), Space: 44.9 MB (12.17%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,5 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
         int profit=0;
         for(int i=1;i<prices.length;i++){
            if(prices[i]>prices[i-1]){
