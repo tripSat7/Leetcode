bc1a44ff75d1e0fd05778c0daf6a947f85a63ef9
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-28 02:26:56 +0530

    Time: 1 ms (92.95%), Space: 44.6 MB (36.09%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int low=Integer.MAX_VALUE;
+        int res=0, prof=0, cp=0, i;
+        for(i=0;i<prices.length-1;i++){
+            if(prices[i]<low){
+                low=prices[i];
+            }
+            cp= prices[i]-low;
+            if(cp>prof && prices[i+1]>prices[i]){
+                prof = cp;
+            }else if(cp>prof && prices[i+1]<prices[i]){
+                res = res + cp;
+                prof=0;
+                low = Integer.MAX_VALUE;
+            }
+        }
+        
+        if(low<Integer.MAX_VALUE && prices[i]>=prices[i-1]){
+            res = res + (prices[i]-low);
+        }
+        return res;
+    }
+}
