39689c308ba0b5e7ddba782ca2e546ea40f7caad
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-29 19:26:29 +0530

    Time: 1 ms (99.39%), Space: 44.1 MB (54.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
-        int slow=0,fast=slow;
+        int slow=0,fast=1;
         
         while(fast<nums.length){
             if(nums[slow]!=nums[fast]){
