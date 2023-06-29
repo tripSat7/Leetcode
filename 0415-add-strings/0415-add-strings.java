8c75ad0e5bd7e344f9a9785486389a686cf65b2d
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-06-29 20:50:51 +0530

    Time: 11 ms (31.47%), Space: 44.4 MB (11.77%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -3,17 +3,19 @@ class Solution {
         int carry = 0;
         int i = num1.length()-1;
         int j = num2.length()-1;
+        char ch1[] = num1.toCharArray();
+        char ch2[] = num2.toCharArray();
         String result = "";
         while(i>=0||j>=0){
             int sum = 0;
             sum+= carry;
             if(i>=0){
-                int n1 = num1.charAt(i)-'0';
+                int n1 = ch1[i]-'0';
                 sum += n1;
                 i--;
             }
             if(j>=0){
-                int n2 = num2.charAt(j)-'0';
+                int n2 = ch2[j]-'0';
                 sum+=n2;
                 j--;
             }
