99e283a2d1c37ae6c6a3a8d9f60443183621c160
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-07-02 11:38:38 +0530

    Time: 1 ms (100.00%), Space: 41.5 MB (60.34%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -7,30 +7,32 @@ class Solution {
     }
     
     public String addBinaryResult(String a, String b){
-        String result="";
+        StringBuilder result= new StringBuilder();
         int carry=0;
+        char ch1[] = a.toCharArray();
+        char ch2[] = b.toCharArray();
         int len1=a.length()-1;
         int len2=b.length()-1;
          while(len1>=0 && len2>=0){
-            int s1=a.charAt(len1)-'0';
-            int s2=b.charAt(len2)-'0';
+            int s1=ch1[len1]-'0';
+            int s2=ch2[len2]-'0';
             int rem= (s1+s2+carry)%2;
             carry=(s1+s2+carry)/2;
-            result=rem+result;
+            result.append(rem);
             len1--;len2--;
         }
         
         while(len1>=0){
-            int s1=a.charAt(len1)-'0';
+            int s1=ch1[len1]-'0';
             int rem= (s1+carry)%2;
             carry=(s1+carry)/2;
-            result=rem+result;
+            result.append(rem);
             len1--;
         }
         if(carry!=0){
-            result=carry+result;
+            result.append(carry);
         }
         
-        return result;
+        return result.reverse().toString();
     }
 }
