776b97f2ac1e26f05589dd54cf63095e2b1264ea
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-07-04 12:45:45 +0530

    Time: 9 ms (99.58%), Space: 43.2 MB (22.37%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        
+        int rev=0, temp=x;
+        while(temp>0){
+            int rem = temp%10;
+            rev = rev*10 + rem;
+            temp = temp/10;
+        }
+        
+        return rev==x;
+    }
+}
