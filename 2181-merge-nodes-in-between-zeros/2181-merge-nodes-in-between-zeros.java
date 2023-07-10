723765da4e72f0f550b2de9124544dc20008c200
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-07-10 21:21:45 +0530

    Time: 6 ms (94.68%), Space: 95.2 MB (16.68%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -13,22 +13,21 @@ class Solution {
         ListNode res = head;
         ListNode temp = head.next;
         ListNode resTemp = res;
-        int sum=0;
         while(temp!=null){
-            if(temp.val!=0){
+            int sum=0;
+            while(temp.val!=0){
                 sum+=temp.val;
-            }else{
-                if(temp.next==null){
-                    resTemp.val = sum;
-                    resTemp.next = null;
-                }else{
-                    resTemp.val = sum;
-                    resTemp.next = temp;
-                    resTemp = resTemp.next;
-                    sum=0;
-                }
+                temp=temp.next;
             }
-            temp=temp.next;
+            resTemp.val = sum;
+            if(temp.next==null){
+                resTemp.next = null;
+            }
+            else{
+                resTemp.next = temp;
+                resTemp = resTemp.next;
+            }
+            temp = temp.next;
         }
         
         return res;
