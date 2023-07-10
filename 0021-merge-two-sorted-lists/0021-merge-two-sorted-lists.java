84b892b393ae06114878c8e2d62cdf772d4615f6
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-07-10 11:16:47 +0530

    Time: 20 ms (6.44%), Space: 43.5 MB (6.76%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1 == null && list2 == null){
+            return null;
+        }else if(list1==null){
+            return list2;
+        }else if(list2==null){
+            return list1;
+        }
+        
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode resHead = new ListNode();
+        
+        if(temp1.val>temp2.val){
+            System.out.println(temp2.val+"::");
+            resHead = temp2;
+            temp2 = temp2.next;
+        }else{
+            System.out.println(temp1.val+"||");
+            resHead = temp1;
+            temp1 = temp1.next;
+        }
+        ListNode temp3 = resHead;
+        while(temp1!=null && temp2!=null){
+            if(temp1.val > temp2.val){
+                System.out.println(temp2.val+"::");
+                temp3.next = temp2;
+                temp2= temp2.next;
+                temp3 = temp3.next;
+            }else{
+                System.out.println(temp1.val+"||");
+                temp3.next = temp1;
+                temp1 = temp1.next;
+                temp3 = temp3.next;
+            }
+        }
+        
+        if(temp1==null){
+            while(temp2!=null){
+                temp3.next = temp2;
+                temp2 = temp2.next;
+                temp3 = temp3.next;
+            }
+        }else{
+            while(temp1!=null){
+                temp3.next = temp1;
+                temp1 = temp1.next;
+                temp3 = temp3.next;
+            }
+        }
+        
+        return resHead;
+    }
+}
