b701fa2c1d07319aa1f93f23fca97ee385764b2e
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-07-16 00:15:44 +0530

    Time: 18 ms (97.45%), Space: 46.3 MB (95.28%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,21 +1,76 @@
+class ListNode{
+    ListNode next;
+    int key, val;
+    
+    ListNode(int key, int val){
+        this.key = key;
+        this.val = val;
+    }
+}
+
 class MyHashMap {
 
-    int valueMap[]; 
+    ListNode hashMap[];
+    int size;
     public MyHashMap() {
-        valueMap = new int[1000001];
+        size = 10000;//10^4 is the max no of calls being made
+        hashMap = new ListNode[size];
+    }
+    
+    public int hash(int key){
+        return key%size;
     }
     
     public void put(int key, int value) {
-        valueMap[key] = value+1;
+        int index = hash(key);
+        if(hashMap[index]==null){
+            hashMap[index] = new ListNode(key,value); 
+        }else{
+            ListNode temp = hashMap[index];
+            while(true){
+                if(temp.key==key){
+                    temp.val = value;
+                    return;
+                }
+                if(temp.next==null){
+                    break;
+                }
+                temp = temp.next;
+            }
+            temp.next = new ListNode(key,value);
+        }
     }
     
     public int get(int key) {
+        int index = hash(key);
+        ListNode temp = hashMap[index];
+        while(temp!=null){
+            if(temp.key==key){
+                return temp.val;
+            }
+            temp = temp.next;
+        }
         
-        return valueMap[key]-1;
+        return -1;
     }
     
     public void remove(int key) {
-        valueMap[key] = 0;
+        int index = hash(key);
+        ListNode temp = hashMap[index];
+        ListNode prev = null;
+        
+        while(temp!=null){
+            if(temp.key==key){
+                if(prev == null){
+                    hashMap[index] = temp.next;
+                }else{
+                    prev.next = temp.next;
+                }
+                return;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
     }
 }
 
