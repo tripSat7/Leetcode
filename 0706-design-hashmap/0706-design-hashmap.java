bc5ac35dc1e1041bad68be26c37830af8e88535b
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-07-15 23:43:34 +0530

    Time: 24 ms (66.75%), Space: 54.5 MB (25.60%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,25 +1,21 @@
 class MyHashMap {
 
-    boolean keyMap[];
     int valueMap[]; 
     public MyHashMap() {
-        keyMap = new boolean[1000001];
         valueMap = new int[1000001];
     }
     
     public void put(int key, int value) {
-        keyMap[key] = true;
-        valueMap[key] = value;
+        valueMap[key] = value+1;
     }
     
     public int get(int key) {
         
-        return keyMap[key]==true?valueMap[key]:-1;
+        return valueMap[key]-1;
     }
     
     public void remove(int key) {
-        keyMap[key] = false;
-        valueMap[key] = -1;
+        valueMap[key] = 0;
     }
 }
 
