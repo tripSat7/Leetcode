9608fb90de3de37ec60db133dba1c9ce680e768b
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-08-05 16:05:23 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (94.92%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,23 +1,18 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long low=1, high=n;
-        
+        long low=1,high=n;
+        while(low<=high)
+        {
+            long mid=low+(high-low)/2;
+            long complete=(mid*(mid+1))/2;
 
-        while(high>=low){
-            long mid = low+(high-low)/2;
-            long sum = (mid*(mid+1))/2;
-            System.out.println(sum);
-            
-            if(sum==n){
-                return (int)mid;
-            }
-            else if(sum>n){
-                high = mid-1;
-            }else{
-                low = mid+1;
-            }
-            
+            if(complete==n)
+            return (int) mid;
+            else if(complete>n)
+            high=mid-1;
+            else
+            low=mid+1;
         }
-        return (int)high;
+        return (int) high;
     }
 }
