8b72068d394a3a2f27d7f2e0ec74497da8ff9762
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-08-31 11:57:08 -0400

    Time: 1 ms (9.16%), Space: 40.3 MB (94.11%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,18 +13,23 @@
  *     }
  * }
  */
-class Solution {
+class Solution {    
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        helper(root, res);
-        return res;
-    }
-
-    public void helper(TreeNode root, List<Integer> res) {
-        if (root != null) {
-            res.add(root.val);
-            helper(root.left, res);
-            helper(root.right, res);
+        List<Integer> answer = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        
+        // Note that we add currNode's right child to the stack first.
+        while (!stack.isEmpty()) {
+            TreeNode currNode = stack.peek();
+            stack.pop();
+            if (currNode != null) {
+                answer.add(currNode.val);
+                stack.add(currNode.right);
+                stack.add(currNode.left);
+            }
         }
+        
+        return answer;
     }
 }
