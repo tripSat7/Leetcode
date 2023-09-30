ae0cd610e8d6adcd2d566250c975f7fee4d8f626
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-09-30 14:16:41 -0400

    Time: 2 ms (89.55%), Space: 42.8 MB (85.38%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,27 +1,23 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-       Stack<Integer> stack = new Stack<>();
-        int popIndex = 0;
-        for (int i=0; i < pushed.length; i++) {
-            while (i < pushed.length && pushed[i] != popped[popIndex]) {
-                stack.push(pushed[i]);
-                i++;
-            }
-            if (i < pushed.length && pushed[i] == popped[popIndex]) {
-                popIndex++;
-            }
-            while (!stack.isEmpty() && stack.peek() == popped[popIndex]) {
-                stack.pop();
-                popIndex++;
+        Stack<Integer> stk = new Stack<>();
+        int j = 0;
+        for(int i: pushed){
+            stk.push(i);
+            while((j<popped.length && !stk.isEmpty()) && stk.peek() == popped[j]){
+                stk.pop();
+                j++;
             }
         }
         
-        if (popIndex == popped.length){
+        while((j<popped.length && !stk.isEmpty()) && stk.peek() == popped[j]){
+            stk.pop();
+            j++;
+        }
+        if(j == popped.length && stk.isEmpty()){
             return true;
-        }else{
-            return false;    
         }
-            
         
+        return false;
     }
 }
