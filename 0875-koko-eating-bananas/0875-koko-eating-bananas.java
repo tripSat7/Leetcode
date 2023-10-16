ca79ee1986a554e9c51bbd33295db8fd9869950e
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-10-16 19:47:16 -0400

    Time: 12 ms (85.69%), Space: 44.8 MB (8.50%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,32 +1,44 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1, high = max(piles), ans = 0;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            int time = getTime(mid, piles);
-            if(time <= h){
-                high = mid - 1;
-                ans = mid;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+
+        for(int i : piles){
+            if(i <min){
+                min = i;
             }
-            else{
-                low = mid + 1;
+            if(i>max){
+                max = i;
+            }
+        }
+
+        int low=1;
+        int high=max;
+        int ans = Integer.MAX_VALUE;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            long hours = hoursTaken(mid, piles);
+            if(hours > (long)h){
+                low = mid+1;
+            }else{
+                high = mid-1;
+                if(mid < ans){
+                    ans = mid;
+                }
             }
         }
         return ans;
     }
-    public int max(int[] piles){
-        int maxi = Integer.MIN_VALUE;
-        for(int i = 0; i < piles.length; i++){
-            maxi = Math.max(piles[i], maxi);
-        }
-        return maxi;
-    }
 
-    public int getTime(int mid, int[] piles){
-        int totalHours = 0;
-        for(int i = 0; i < piles.length; i++){
-            totalHours += Math.ceil((double)piles[i]/(double)mid);
+    public long hoursTaken(int k, int[] piles){
+        long hours=0;
+        for(int i : piles){
+            if(i%k == 0){
+                hours += (long)i/k;
+            }else{
+                hours += (long)i/k + 1;
+            }
         }
-        return totalHours;
+        return hours;
     }
 }
