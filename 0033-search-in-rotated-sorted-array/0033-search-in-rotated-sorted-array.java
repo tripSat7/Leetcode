b1b27bc1447307b4ffd143914caec6914bf9da61
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-10-24 11:43:08 -0400

    Time: 8 ms (5.34%), Space: 41.2 MB (22.09%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int idx = minIndex(nums);
+        //System.out.println(idx);
+        int left = 0, right = nums.length - 1;
+        if(target == nums[idx]){
+            return idx;
+        }
+        else if(target <= nums[right] && target > nums[idx]){
+            left = idx + 1;
+        }
+        else{
+            right = idx - 1;
+        }
+        //System.out.println(left+"||"+right);
+        while(left < right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        System.out.println(left+"||"+right);
+        if(right > -1 && nums[right] == target){
+            return right;
+        }
+        
+        return -1;
+    }
+    
+    public int minIndex(int nums[]){
+        
+        int n = nums.length;
+        int left = 0, right = n-1;
+        while(left < right){
+            int mid = left+(right - left)/2;
+            if(mid > 0 && nums[mid]<nums[mid-1]){
+                return mid;
+            }
+            else if(nums[mid] >= nums[left] && nums[mid] > nums[right]){
+                left = mid+1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+}
