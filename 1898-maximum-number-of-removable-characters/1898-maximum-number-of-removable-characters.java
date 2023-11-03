627a4af010670475e7d48ddc7b87de99b060636d
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-11-03 11:29:27 -0400

    Time: 40 ms (96.48%), Space: 55.6 MB (84.38%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.java
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.java
@@ -1,38 +1,40 @@
 class Solution {
     public int maximumRemovals(String s, String p, int[] removable) {
         
-        char[] sArr = s.toCharArray();
-        char[] pArr = p.toCharArray();
-        int start = 0;
-        int end = removable.length-1;
-        
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            for(int i=start;i<=mid;i++){
-                sArr[removable[i]] = '*';
+        int left = 0, right = removable.length-1;
+        char ch1[] = s.toCharArray();
+        char ch2[] = p.toCharArray();
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            for(int i = 0; i <= mid; i++) ch1[removable[i]] = '|';
+            
+            if(isSubsequence(ch1, ch2)){
+                left = mid + 1;
             }
-            if(isSubSequence(sArr,pArr))
-                start=mid+1;
             else{
-                for(int i=start;i<=mid;i++){
-                sArr[removable[i]] = s.charAt(removable[i]); 
+                for(int i = left; i<= mid; i++){
+                    ch1[removable[i]] = s.charAt(removable[i]);
+                }
+                right = mid - 1;
             }
-            end = mid-1;
-            } 
         }
-        return end+1;
         
+        return right+1;
     }
-    private boolean isSubSequence(char[] sArr,char[] pArr)
-    {
-        int i=0,j=0;
-        while(i<sArr.length && j<pArr.length){
-            if(sArr[i]==pArr[j]){
-                i++;
+    
+    
+    public boolean isSubsequence(char ch1[], char ch2[]){
+        
+        int sLen = ch1.length, pLen = ch2.length;
+        int i = 0, j = 0;
+        
+        while(i<sLen && j<pLen){
+            if(ch1[i] == ch2[j]){
                 j++;
             }
-            else i++;
+            i++;
         }
-        return j==pArr.length;
+        
+        return j == pLen;
     }
 }
