a7fc9914641728b072a9d82900dbb0c101e0211d
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-11-03 11:20:04 -0400

    Time: 36 ms (100.00%), Space: 55.6 MB (84.38%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.java
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.java
@@ -1,37 +1,38 @@
 class Solution {
     public int maximumRemovals(String s, String p, int[] removable) {
         
-        int left = 0, right = removable.length-1;
-        char ch2[] = p.toCharArray();
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            char ch1[] = s.toCharArray();    
-            for(int i = 0; i <= mid; i++) ch1[removable[i]] = '|';
-            
-            if(isSubsequence(ch1, ch2)){
-                left = mid + 1;
+        char[] sArr = s.toCharArray();
+        char[] pArr = p.toCharArray();
+        int start = 0;
+        int end = removable.length-1;
+        
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            for(int i=start;i<=mid;i++){
+                sArr[removable[i]] = '*';
             }
+            if(isSubSequence(sArr,pArr))
+                start=mid+1;
             else{
-                right = mid - 1;
+                for(int i=start;i<=mid;i++){
+                sArr[removable[i]] = s.charAt(removable[i]); 
             }
+            end = mid-1;
+            } 
         }
+        return end+1;
         
-        return right+1;
     }
-    
-    
-    public boolean isSubsequence(char ch1[], char ch2[]){
-        
-        int sLen = ch1.length, pLen = ch2.length;
-        int i = 0, j = 0;
-        
-        while(i<sLen && j<pLen){
-            if(ch1[i] == ch2[j]){
+    private boolean isSubSequence(char[] sArr,char[] pArr)
+    {
+        int i=0,j=0;
+        while(i<sArr.length && j<pArr.length){
+            if(sArr[i]==pArr[j]){
+                i++;
                 j++;
             }
-            i++;
+            else i++;
         }
-        
-        return j == pLen;
+        return j==pArr.length;
     }
 }
