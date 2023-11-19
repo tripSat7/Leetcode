c94c4813cc857e2c0300e14530d46f3004ca3d59
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-11-19 13:36:16 -0500

    Time: 0 ms (100.00%), Space: 43.4 MB (28.55%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,32 +14,22 @@
  * }
  */
 class Solution {
+    int postJ;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        
-    return buildBinaryTree(postorder, 0 , postorder.length - 1, inorder, 0, inorder.length - 1);
+        postJ = postorder.length - 1;
+        return solve(inorder, postorder, 0, inorder.length - 1);
     }
     
-    public TreeNode buildBinaryTree(int[] postorder, int poststart, int postEnd, int inorder[], int inStart, int inEnd){
-        if(poststart > postEnd || inStart > inEnd){
-            return null;
+    TreeNode solve(int[] inorder, int[] postorder, int inI, int inJ) {
+        if(inI > inJ) return null;
+        TreeNode node = new TreeNode(postorder[postJ]);
+        int k;
+        for(k = inJ; k >= inI; k--) {
+            if(inorder[k] == postorder[postJ]) break;
         }
-        int val = postorder[postEnd];
-        TreeNode root = new TreeNode(val);
-        
-        int offset = inStart;
-        
-        while(offset < inEnd){
-            if(inorder[offset] == val){
-                break;
-            }
-            
-            offset++;
-        }
-        
-        root.left = buildBinaryTree(postorder, poststart, poststart + offset - inStart -1, inorder, inStart, offset - 1);
-        root.right = buildBinaryTree(postorder, poststart + offset - inStart , postEnd - 1, inorder, offset+1, inEnd);
-        
-        
-        return root;
+        postJ--;
+        node.right = solve(inorder, postorder, k + 1, inJ);
+        node.left = solve(inorder, postorder, inI, k - 1);
+        return node;
     }
 }
