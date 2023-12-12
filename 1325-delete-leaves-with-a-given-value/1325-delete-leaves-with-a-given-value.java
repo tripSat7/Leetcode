997529cc124911248d6239d7f19063a091706651
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-12-12 08:55:11 -0500

    Time: 0 ms (100.00%), Space: 43 MB (59.10%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        
+        return helper(root, target);
+    }
+    
+    public TreeNode helper(TreeNode root, int target){
+        
+        if(root == null){
+            return null;
+        }
+        
+        root.left = helper(root.left, target);
+        root.right = helper(root.right, target);
+        if(root.val == target && root.left == null && root.right == null){
+            return null;
+        }
+        
+        return root;
+    }
+}
