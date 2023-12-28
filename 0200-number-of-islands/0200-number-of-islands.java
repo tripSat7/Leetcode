e1504385afc225f6e3f230f5a23bb53f6d3232f6
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-12-27 20:17:48 -0500

    Time: 3 ms (74.71%), Space: 49.8 MB (24.70%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,57 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int sol = 0;
-        int[][] vis = new int[m][n];
-        for (int r=0;r<m;r++)
-        {
-            for (int c=0;c<n;c++)
-            {
-                if(grid[r][c]=='1' && vis[r][c]==0)
-                {
-                    sol++;
-                    bfs(r,c,vis,grid);
+        int rowLength = grid.length;
+        int columnLength = grid[0].length;
+        int noOfIsnlands =0;
+
+        for(int r=0; r< rowLength;r++){
+            for(int c=0; c< columnLength;c++){
+                if(grid[r][c] == '1'){
+                    checkIsland(grid,r,c);
+                    noOfIsnlands++;
                 }
             }
         }
-        return sol;
+       return noOfIsnlands;
     }
-    public void bfs(int r,int c,int[][] vis,char[][] grid)
-    {
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        vis[r][c]=1;
-        q.add(new Pair(r,c));
-        int[] arow={-1,0,0,1};
-        int[] acol={0,-1,+1,0};
-        while(!q.isEmpty())
-        {
-            int row=q.peek().r;
-            int col=q.peek().c;
-            q.remove();
-            for (int i=0;i<arow.length;i++)
-            {
-                int row1=row+arow[i];
-                int col1=col+acol[i];
-                if(row1>=0 && row1<m && col1>=0 && col1<n && 
-                grid[row1][col1]=='1' && vis[row1][col1]==0)
-                {
-                    q.add(new Pair(row1,col1));
-                    vis[row1][col1]=1;
-                }
+
+    public void checkIsland(char[][] grid, int row, int column){
+        int rowLength = grid.length;
+        int columnLength = grid[0].length;
+
+        if( row < 0 || column <0 || row >= rowLength || column >= columnLength ||
+            grid[row][column] == '0'){
+                return;
             }
-        }
-    }
-}
-class Pair
-{
-    int r;
-    int c;
-    Pair(int r,int c)
-    {
-        this.r=r;
-        this.c=c;
+        
+        grid[row][column]='0';
+        checkIsland(grid, row+1, column);
+        checkIsland(grid, row-1, column);
+        checkIsland(grid, row, column-1);
+        checkIsland(grid, row, column+1);
     }
 }
