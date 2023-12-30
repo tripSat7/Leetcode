e914bbd32b4d56f1fe38ac043d558c45f94c4bab
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-12-30 04:27:25 -0500

    Time: 5 ms (9.58%), Space: 42.2 MB (24.82%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -2,49 +2,38 @@
 
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] visited = grid;
         Queue<int[]> q = new LinkedList<>();
-        int countFreshOrange = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 2) {
-                    q.offer(new int[] {i, j});
-                }
-                if (visited[i][j] == 1) {
-                    countFreshOrange++;
-                }
+        int fresh = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 2){
+                    q.add(new int[]{i,j});  
+                } 
+                else if(grid[i][j] == 1){
+                    fresh++;  
+                } 
             }
         }
-        if (countFreshOrange == 0)
-            return 0;
-        if (q.isEmpty())
-            return -1;
-        
-        int minutes = -1;
-        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
-        while (!q.isEmpty()) {
+        int res = 0;
+        int[][] search = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty() && fresh > 0){
             int size = q.size();
-            while (size-- > 0) {
-                int[] cell = q.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for (int[] dir : dirs) {
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
-                        visited[i][j] = 2;
-                        countFreshOrange--;
-                        q.offer(new int[] {i, j});
+            res++;
+            for(int i = 0; i < size; i++){
+                int[] orange = q.poll();
+                int rX = orange[0];
+                int rY = orange[1];
+                for(int[] adj : search){
+                    int x = rX + adj[0];
+                    int y = rY + adj[1];
+                    if(x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 1){
+                        grid[x][y] = 2;
+                        q.offer(new int[]{x,y});
+                        fresh--;
                     }
                 }
             }
-            minutes++;
         }
-        
-        if (countFreshOrange == 0)
-            return minutes;
-        return -1;
+        return fresh == 0 ? res : -1;
     }  
 }
