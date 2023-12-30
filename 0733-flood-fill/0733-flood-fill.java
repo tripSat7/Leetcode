2f2f81eecddb597656d98cc8b863a45fc4ce870f
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2023-12-29 21:23:57 -0500

    Time: 1 ms (47.14%), Space: 45 MB (7.08%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        if(image[sr][sc] != color){
+            dfs(image, sr, sc, image[sr][sc], color);
+        }
+        return image;
+    }
+    
+    public void dfs(int[][] image, int sr, int sc, int curr, int color){
+        int rowLength = image.length;
+        int columnLength = image[0].length;
+        
+        if( sr < 0 || sc < 0 || sr >= rowLength || sc >= columnLength ||
+            image[sr][sc] != curr){
+                return;
+        }
+        
+        image[sr][sc] = color;
+        dfs(image, sr-1, sc, curr, color);
+        dfs(image, sr+1, sc, curr, color);
+        dfs(image, sr, sc+1, curr, color);
+        dfs(image, sr, sc-1, curr, color);
+        
+    }
+}
