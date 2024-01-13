dea9f8b31b2c05478620ae1c2396d4bf1609b5ae
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2024-01-12 21:10:54 -0500

    Time: 2 ms (76.38%), Space: 44.6 MB (24.60%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,61 +1,22 @@
-class Pair{
-    int first;
-    int second;
-    
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
-    int max = 0;
-    public int maxAreaOfIsland(int[][] grid) {
-        
-        int m = grid.length;
-        int n = grid[0].length;
-        int vis[][] = new int[m][n];
-        int arow[] = {-1,0,+1,0};
-        int acol[] = {0,-1,0,+1};
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(vis[i][j] == 0 && grid[i][j] == 1){
-                    bfs(i, j, grid, vis, arow, acol);
-                }
-            }
-        }
-        
-        return max;
-    }
     
-    public void bfs(int row, int col, int[][] grid, int vis[][], int arow[], int acol[]){
-        vis[row][col] = 1;
-        int m = grid.length;
-        int n = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(row,col));
-        int count = 1;
-        while(!q.isEmpty()){
-            int qr = q.peek().first;
-            int qc = q.peek().second;
-            q.remove();
-            
-            for(int i = 0; i<4; i++){
-                int nr = qr + arow[i];
-                int nc = qc + acol[i];
-                
-                if(nr >= 0 && nr < m && nc >= 0 && nc < n && vis[nr][nc] == 0 && grid[nr][nc] == 1){
-                    count++;
-                    vis[nr][nc] = 1;
-                    q.add(new Pair(nr,nc));
-                }
+    public int area(int r, int c, int[][] grid, boolean[][] seen) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+                seen[r][c] || grid[r][c] == 0)
+            return 0;
+        seen[r][c] = true;
+        return (1 + area(r+1, c, grid, seen) + area(r-1, c, grid, seen)
+                  + area(r, c-1, grid, seen) + area(r, c+1, grid, seen));
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        boolean[][] seen = new boolean[grid.length][grid[0].length];
+        int ans = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                ans = Math.max(ans, area(r, c, grid, seen));
             }
         }
-        
-        if(count > max){
-            max = count;
-        }
-        
+        return ans;
     }
 }
