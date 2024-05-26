5bfe7bcf6ff40ae82ad09739b789b1c4adb6dc44
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2024-05-26 08:59:21 -0400

    Time: 1 ms (100.00%), Space: 42.4 MB (31.55%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,34 +1,32 @@
 class Solution {
     public int myAtoi(String s) {
+        int res=0;
+        int i=0;
+        int flag=1;
+        int max=Integer.MAX_VALUE;
+        int min=Integer.MIN_VALUE;
         
-        char ch[] = s.trim().toCharArray();
-        Integer sign = 1;
-        int i = 0;
-        Integer res = 0;
-        
-        if(ch.length > 0 && ((ch[0] >= 'A' && ch[0] <= 'Z') || (ch[0] >= 'a' && ch[0] <= 'z') || ch[0] =='.')){
+        if(s.length()==0){
             return 0;
         }
-        else if(ch.length > 0 &&(ch[0] == '-' || ch[0] == '+')){
-            if(ch[0] == '-'){
-                sign = sign * -1;
+        while(i<s.length() && s.charAt(i)==' '){
+            i++;
+        }
+
+        if(i<s.length() && (s.charAt(i)=='-'|| s.charAt(i)=='+')){
+            if(s.charAt(i)=='-'){
+                flag=-1;
             }
             i++;
         }
-        
-        
-        while (i < ch.length && ch[i] >= '0' && ch[i] <= '9') {
-			// check if we will go over the max
-			if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && ch[i] - '0' > 7)) {
-				if (sign == -1) {
-					return Integer.MIN_VALUE;
-				}
-				return Integer.MAX_VALUE;
-			}
-			
-			// update res
-			res = res * 10 + ( ch[i++] - '0');
-		}
-		return sign * res;
+        while(i<s.length() && s.charAt(i)-'0'<=9 && s.charAt(i)-'0'>=0){
+            int digit = s.charAt(i)-'0';
+            if(res>max/10 || (res==max/10 && digit>max%10)){
+                return (flag == 1) ? max : min;            
+            }
+            res=res*10+digit;
+            i++;
+        }
+        return res*flag;
     }
 }
