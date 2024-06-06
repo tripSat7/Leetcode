3e176e0b2f0f57ec05ed3dabd18ab4ae3e6ef059
tripSat7 <117186874+tripSat7@users.noreply.github.com>
2024-06-06 09:17:28 -0400

    Time: 3 ms (97.05%), Space: 43.9 MB (88.46%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,9 +1,20 @@
 class Solution {
     public String intToRoman(int num) {
-        String M[] = {"", "M", "MM", "MMM"};
-        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        String[] ones = new String[]{"","I","II","III","IV","V", "VI","VII","VIII","IX"};
+        String[] tens = new String[]{"","X", "XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hundreds = new String[]{"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] thousands = new String[]{"","M","MM","MMM"};
+
+       StringBuilder sb = new StringBuilder();
+       String thousand = thousands[num/1000];
+       String hundred = hundreds[(num%1000)/100];
+       String ten = tens[(num%100)/10];
+       String one = ones[num%10];
+        sb.append(thousand);
+        sb.append(hundred);
+        sb.append(ten);
+        sb.append(one);
+
+        return sb.toString();
     }
 }
