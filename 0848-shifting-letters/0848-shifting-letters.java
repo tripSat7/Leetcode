d6817b523f4f11c88657ab3f7df83efba2e2d791
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-06-15 05:38:15 -0400

    Time: 8 ms (68.48%), Space: 55.2 MB (93.48%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -1,11 +1,11 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        StringBuilder ans = new StringBuilder(s);
+        char arr[] = s.toCharArray();
         long shift=0;
         for (int i = s.length()-1; i >=0 ; i--){
-            ans.setCharAt(i, (char)((s.charAt(i) - 'a' + (shift+shifts[i]) % 26) % 26 + 'a'));
+            arr[i] =  (char)((s.charAt(i) - 'a' + (shift+shifts[i]) % 26) % 26 + 'a');
             shift+=shifts[i];
         }
-        return ans.toString();
+        return new String(arr ,0,arr.length);
     }
 }
