e62b4bb4b123bbe2e6e3f7d26e96a41985fe03e7
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-06-26 11:53:34 -0400

    Time: 26 ms (92.69%), Space: 51.9 MB (43.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,46 +1,42 @@
 class Solution {
-    private int[] nums;
-    private List<List<Integer>> result;
-    private int l;
-    
     public List<List<Integer>> threeSum(int[] nums) {
-        result = new ArrayList<>();
-        l = nums.length;
-        Arrays.sort(nums);
-        this.nums = nums;
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < l - 2; i++) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            if (nums[i] > 0) {
+            if(nums[i] > 0){
                 break;
             }
-            twoSum(i);
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
         }
-
-        return result;
-    }
-
-    private void twoSum(int i) {
-        int j = i + 1, k = l - 1, sum;
-
-        while (j < k) {
-            sum = nums[i] + nums[j] + nums[k];
-            if (sum > 0) {
-                k--;
-                continue;
-            }
-            if (sum < 0) {
-                j++;
-                continue;
-            }
-            result.add(List.of(nums[i], nums[j], nums[k]));
-            j++;
-            k--;
-            while (j < k && nums[j] == nums[j - 1]) {
-                j++;
-            }
-        }
+        return ans;
     }
 }
