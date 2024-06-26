6d11b0d255aee7f362087823c3e2ebdaa02019fd
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-06-26 11:52:54 -0400

    Time: 25 ms (95.79%), Space: 51.2 MB (72.48%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,40 +1,46 @@
 class Solution {
+    private int[] nums;
+    private List<List<Integer>> result;
+    private int l;
+    
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
+        result = new ArrayList<>();
+        l = nums.length;
         Arrays.sort(nums);
+        this.nums = nums;
 
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < l - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
+            if (nums[i] > 0) {
+                break;
+            }
+            twoSum(i);
+        }
 
-            int j = i + 1;
-            int k = nums.length - 1;
+        return result;
+    }
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+    private void twoSum(int i) {
+        int j = i + 1, k = l - 1, sum;
 
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
-                }
+        while (j < k) {
+            sum = nums[i] + nums[j] + nums[k];
+            if (sum > 0) {
+                k--;
+                continue;
+            }
+            if (sum < 0) {
+                j++;
+                continue;
+            }
+            result.add(List.of(nums[i], nums[j], nums[k]));
+            j++;
+            k--;
+            while (j < k && nums[j] == nums[j - 1]) {
+                j++;
             }
         }
-        return ans;
     }
 }
