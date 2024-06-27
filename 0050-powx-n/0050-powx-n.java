d509440d70fd459b4dc18329318f544a465d2a07
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-06-27 12:13:30 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (90.91%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,10 +1,11 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n<0) return 1/x * myPow(1/x, -(n+1));
-        if(n==0) return 1;
-        if(n==2) return x*x;
-        if(n%2==0) return myPow( myPow(x, n/2), 2);
-        else return x*myPow( myPow(x, n/2), 2);
+        if(n == 0)
+            return 1;
+        if(n<0){
+            return 1/x * myPow(1/x, -(n + 1));
+        }
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
         
     }
 }
