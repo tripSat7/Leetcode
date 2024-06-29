4881909140d5e560e08dd56ef70040688fcdd40a
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-06-29 11:53:40 -0400

    Time: 1 ms (83.95%), Space: 43 MB (54.31%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        generation(res, "", n, n);
+        return res;
+    }
+    
+    public void generation(List<String> res, String s, int left, int right){
+        if(left == 0 && right == 0){
+            res.add(s);
+        }
+        
+        if(left > 0){
+            generation(res, s+"(", left-1, right);
+        }
+        
+        if(left < right){
+            generation(res, s+")", left, right-1);
+        }
+    }
+}
