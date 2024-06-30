9b650198a8c337ea52481a032c77ecd9646ed7bd
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-06-30 13:24:07 -0400

    Time: 4 ms (96.91%), Space: 44 MB (99.87%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -5,26 +5,25 @@ class Solution {
         int[] count = new int[128];
         List<Character>[] buckets = new List[n + 1];
 
-        for (char c : s.toCharArray()){
+        for(char c : s.toCharArray()){
             count[c]++;
         }
           
-
-        for (int i = 0; i < 128; ++i) {
-          int freq = count[i];
-          if (freq > 0) {
-            if (buckets[freq] == null){
-                buckets[freq] = new ArrayList<>();
+        for(int i = 0; i < 128; i++) {
+            int freq = count[i];
+            if(freq > 0) {
+                if(buckets[freq] == null){
+                    buckets[freq] = new ArrayList<>();
+                }
+                buckets[freq].add((char) i);
             }
-            buckets[freq].add((char) i);
-          }
         }
 
-        for (int freq = n; freq > 0; freq--){
-            if (buckets[freq] != null){
-                for (char c : buckets[freq])
+        for(int freq = n; freq > 0; freq--){
+            if(buckets[freq] != null){
+                for(char c : buckets[freq])
                 {
-                    for (int i = 0; i < freq; ++i)
+                    for(int i = 0; i < freq; ++i)
                     {
                         sb.append(c);
                     }
