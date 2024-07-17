e2cbccdf2c06f521b8e641108c6cec05df764bb7
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-07-17 11:53:35 -0400

    Time: 145 ms (57.37%), Space: 41.7 MB (27.21%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,44 +2,41 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        boolean vis[][] = new boolean[m][n];
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == word.charAt(0)){
-                    if(dfs(board, word, i, j, 0, vis)){
-                        return true;   
-                    }
+        boolean[][] visited = new boolean[m][n];
+        boolean result = false;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    result = backtrack(board, word, visited, i, j, 0);
+                    if (result) return true;
                 }
-            } 
+            }
         }
         
         return false;
     }
     
-    public boolean dfs(char[][] board, String word, int r, int c, int k, boolean vis[][]){
-        
-        if(k == word.length()){
+    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
+        if (index == word.length()) {
             return true;
         }
         
-        if(r < 0 || c < 0 || r >= board.length || c >= board[0].length || vis[r][c] || k >= word.length() || board[r][c] != word.charAt(k)){
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
         
-        //System.out.println(r+":"+c+"||"+word.charAt(k));
+        visited[i][j] = true;
         
-        
-        vis[r][c] = true;
-        if(dfs(board, word, r , c + 1, k + 1, vis) 
-            || dfs(board, word, r + 1, c , k + 1, vis) 
-            || dfs(board, word, r - 1, c , k + 1, vis)
-            || dfs(board, word, r , c - 1, k + 1, vis)
-          ){
+        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
+            backtrack(board, word, visited, i - 1, j, index + 1) ||
+            backtrack(board, word, visited, i, j + 1, index + 1) ||
+            backtrack(board, word, visited, i, j - 1, index + 1)) {
             return true;
         }
         
-        vis[r][c] = false;
+        visited[i][j] = false;
         return false;
     }
 }
