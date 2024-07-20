a4f5927db150b174ed81279f14faa2d60a869101
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-07-19 21:07:16 -0400

    Time: 4 ms (98.62%), Space: 45 MB (82.62%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,26 +1,22 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
         int[] degree = new int[n];
-        Set<String> roadSet = new HashSet<>();
-        
-        for (int[] road : roads) {
+        int[][] connected = new int[n][n];
+
+        for(int[] road : roads){
             degree[road[0]]++;
             degree[road[1]]++;
-            roadSet.add(road[0] + "," + road[1]);
-            roadSet.add(road[1] + "," + road[0]);
+            connected[road[0]][road[1]] = 1;
+            connected[road[1]][road[0]] = 1;
         }
-
         int maxRank = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                int rank = degree[i] + degree[j];
-                if (roadSet.contains(i + "," + j)) {
-                    rank--;
-                }
+
+        for(int i = 0; i < n;  i++){
+            for(int j = i + 1; j < n; j++){
+                int rank = degree[i] + degree[j] - connected[i][j];
                 maxRank = Math.max(maxRank, rank);
             }
         }
-
         return maxRank;
     }
 }
