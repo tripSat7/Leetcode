1ee5af3017f3c2e9c824fb66d76cd0765c3ed7ef
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-07-26 16:43:23 -0400

    Time: 16 ms (90.69%), Space: 46.2 MB (96.90%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -17,9 +17,9 @@ class Solution {
         }
 
         List<Boolean> res = new ArrayList<>();
-        for(int[] querie : queries){
-            int a = querie[0];
-            int b = querie[1];
+        for(int[] query: queries){
+            int a = query[0];
+            int b = query[1];
             res.add(visited[a][b]);
         }
         return res;
@@ -27,12 +27,18 @@ class Solution {
 
     private void dfs(int node, boolean[] vis, List<Integer>[] graph){
         vis[node] = true;
+        // System.out.println(node);
+        // for(int i = 0 ; i < vis.length; i++){
+        //     System.out.print(vis[i]);
+        // }
+        // System.out.println();
         for(int child : graph[node]){
             if(vis[child]) 
             {
                 continue;
             }
-             dfs(child, vis, graph);
+            dfs(child, vis, graph);
+            // System.out.println("dfs");
         }
     }
 }
