40dcff8bd2d4acd55fe837e8c7e59f3e8933a0dc
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-07-31 00:51:25 -0400

    Time: 10 ms (63.06%), Space: 42.3 MB (82.80%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,18 +1,21 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] arr = new int[26];
-        int largestCount = 0, beg = 0, maxlen = 0;
-        char[] ans = s.toCharArray();
         
-        for(int end = 0; end < ans.length; end ++){
-            arr[ans[end] - 'A']++;
-            largestCount = Math.max(largestCount, arr[ans[end] - 'A']);
-            while(end - beg + 1 - largestCount > k){
-                arr[ans[beg] - 'A']--;
+        int[] arr = new int[26];
+        int maxFreq = 0, beg = 0, maxlen = 0;
+
+        for(int end = 0; end < s.length(); end ++){
+            arr[s.charAt(end) - 'A']++;
+            
+            maxFreq = Math.max(maxFreq, arr[s.charAt(end) - 'A']);
+            
+            if(end - beg + 1 - maxFreq > k){ 
+                arr[s.charAt(beg) - 'A']--;
                 beg ++;
             }
+            
+            maxlen = Math.max(maxlen, end - beg + 1);
         }
-        
-        return s.length() - beg;
+        return maxlen;
     }
 }
