a54491796efd4180f9aa47f66fb1d7660ea32404
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-01 20:58:33 -0400

    Time: 1 ms (100.00%), Space: 47.9 MB (37.42%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,21 +1,20 @@
 class Solution {
-    private int slidingWindowAtMost(int[] nums, int goal) {
+    public int give(int[] nums, int goal){
         int n = nums.length;
         int sum = 0, l = 0, r = 0;
-        int count = 0;
-        while(r < n){
+        int c = 0;
+        while(r<n){
             sum += nums[r];
-           while(l <= r && sum > goal){
+           while(sum>goal && l<=r){
             sum -= nums[l];
             l++;
            }
-           count += (r - l + 1);
+           c += (r - l + 1);
            r++;
         }
-        return count;
+        return c;
     }
-
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return slidingWindowAtMost(nums, goal) - slidingWindowAtMost(nums, goal - 1);
+        return give(nums, goal) - give(nums, goal-1);
     }
 }
