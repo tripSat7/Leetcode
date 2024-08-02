590b9dc7a77d4c80b3c3fc18dbbfa4f961951757
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-02 00:07:12 -0400

    Time: 8 ms (59.59%), Space: 55.3 MB (19.01%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,21 +1,19 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return window(nums, k) - window(nums, k-1);
+        return f(nums, k) - f(nums, k-1);
     }
 
-    private int window(int[] nums, int k) {
-        int st = 0, end = 0, sum = 0, count = 0;
-        
-        while (end < nums.length) {
-            sum += (nums[end] & 1);
-            while (sum > k) {
-                sum -= nums[st] & 1;
-                st++;
+    private int f(int[] arr, int k) {
+        int i=0, j=0, odd=0, cnt=0;
+        while (j < arr.length) {
+            odd += (arr[j]&1);
+            while (odd > k) {
+                odd -= (arr[i]&1);
+                i++;
             }
-            count += (end - st + 1);
-            end++;
+            cnt += j-i+1;
+            j++;
         }
-        
-        return count;
+        return cnt;
     }
 }
