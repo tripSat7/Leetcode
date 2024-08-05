11af837f45d69d7bb460e64032171037aa02b85c
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-05 18:00:56 -0400

    Time: 14 ms (80.07%), Space: 45.3 MB (63.03%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -25,10 +25,9 @@ class Solution {
         int dist[][] = new int[m][n];
                 
         for(int i = 0; i < m; i++){
-            // for(int j = 0; j < n; j++){
-            //     dist[i][j] = (int)(1e9);
-            // }
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
+            for(int j = 0; j < n; j++){
+                dist[i][j] = (int)(1e9);
+            }            
         }
         
         q.add(new Pair(1,0,0));
