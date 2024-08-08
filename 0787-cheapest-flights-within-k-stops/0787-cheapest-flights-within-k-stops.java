96031d35091961edda09e4ead1acc7d4d43c9e40
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-07 21:22:51 -0400

    Time: 7 ms (48.39%), Space: 44.9 MB (12.22%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,66 +1,40 @@
-class Pair{
-    int first, second;
-    
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-
-
-class Tuple{
-    int first, second, third;
-    
-    Tuple(int first, int second, int third){
-        this.first = first;
-        this.second = second;
-        this.third = third;
-    }
-}
-
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        
         for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
-        int m = flights.length;
-        for(int i = 0; i < m; i++){
-            adj.get(flights[i][0]).add(new Pair(flights[i][1],flights[i][2]));
+        
+        for(int i = 0; i < flights.length; i++){
+            adj.get(flights[i][0]).add(new int[]{flights[i][1], flights[i][2]});
         }
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        pq.add(new int[]{0, src, 0});
         
         int dist[] = new int[n];
-        for(int i = 0; i < n; i++){
-            dist[i] = (int)(1e9);
-        }
-        
-        Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(0, src, 0));
-        // System.out.println(adj);
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[src] = 0;
-        while(!q.isEmpty()){
-            Tuple t = q.remove();
-            int stops = t.first;
-            int source = t.second;
-            int price = t.third;
-            
-            if(stops > k){
-                continue;
-            }
-            //System.out.println(source);
-            
-            for(Pair itr : adj.get(source)){
-                int ew = price + itr.second;
-                
-                if(ew < dist[itr.first] && stops <= k){
-                    // System.out.println(source+"||"+itr.first+"::"+ew);
-                    dist[itr.first] = ew;
-                    q.add(new Tuple(stops + 1, itr.first, ew));
+        
+        while(!pq.isEmpty()){
+            int curr[] = pq.poll();
+            int stops = curr[0];
+            int s = curr[1];
+            int price = curr[2];
+            // System.out.println("--"+s+"||"+stops+"||"+price);
+            for(int[] nodes : adj.get(s)){
+                int ew = nodes[1];
+                int d = nodes[0];
+                // System.out.println(d+":"+ew);
+                if(stops < k+1 && ew + price < dist[d]){
+                    dist[d] = ew + price;
+                    pq.add(new int[]{stops + 1, d, dist[d]});
                 }
             }
         }
         
-        return dist[dst] == (1e9) ? -1 : dist[dst];
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 }
