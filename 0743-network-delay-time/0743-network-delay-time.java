a788223b9a2025fb54a51efdec7c8058a346654a
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-08 10:00:47 -0400

    Time: 10 ms (85.33%), Space: 48.6 MB (46.96%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,65 +1,51 @@
-class Pair{
-    int first, second;
-    
-    Pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
         
-        int m = times.length;
-        for(int i = 0; i < n + 1; i++){
+        for(int i = 0; i <= n; i++){
             adj.add(new ArrayList<>());
         }
         
-        for(int i = 0; i < m; i++){
-            adj.get(times[i][0]).add(new Pair(times[i][1], times[i][2]));
+        for(int i = 0; i < times.length; i++){
+            adj.get(times[i][0]).add(new int[]{times[i][1], times[i][2]});
         }
         
         int dist[] = new int[n+1];
-        for(int i = 1; i < n+1; i++){
-            dist[i] = (int)(1e9);
-        }
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[k] = 0;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(k,0));
         
-        while(!q.isEmpty()){
-            Pair p = q.remove();
-            int node = p.first;
-            int ew = p.second;
-            // System.out.println(node+"||"+ew);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.add(new int[]{0, k});
+        
+        while(!pq.isEmpty()){
+            int curr[] = pq.poll();
+            int src = curr[1];
+            int t = curr[0];
             
-            for(Pair itr : adj.get(node)){
-                int time = ew + itr.second;
-                int dest = itr.first;
-                // System.out.println("to "+ dest +" : "+time);
-                // System.out.println(dist[dest]);
-                if(time < dist[dest]){
-                    
-                    dist[dest] = time;
-                    q.add(new Pair(dest,time));
+            for(int[] nodes : adj.get(src)){
+                int dest = nodes[0];
+                int time = nodes[1];
+                
+                if(time + t < dist[dest]){
+                    dist[dest] = time + t;
+                    pq.add(new int[]{dist[dest], dest});
                 }
             }
         }
         
-        int res = 0;
+        int min = 0;
         
-        for(int i = 1; i < n + 1; i++){
-            if(dist[i] == (1e9)){
+        for(int i = 1; i <=n; i++){
+            
+            if(dist[i] == Integer.MAX_VALUE){
                 return -1;
             }
-            // System.out.println(i+":"+dist[i]);
-            if(dist[i] > res){
-                res = dist[i];
-            }
+            
+            min = Math.max(min, dist[i]);
         }
         
-        return res;
+        
+        return min;
     }
 }
