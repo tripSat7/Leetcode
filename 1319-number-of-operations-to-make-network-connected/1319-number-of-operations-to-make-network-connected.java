96f53cfc4d17d8fa6a70e8ef5727b46fe5c136ad
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-21 14:15:06 -0400

    Time: 19 ms (25.74%), Space: 61 MB (66.65%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,41 +1,73 @@
+class DisjointSet{
+    List<Integer> size = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    
+    public int findUltParent(int node){
+       
+        if(node == parent.get(node)){
+            return node;
+        }
+        
+        int ulp = findUltParent(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    } 
+    
+    public void unionBySize(int u, int v){
+        int ultP_u = findUltParent(u);
+        int ultP_v = findUltParent(v);
+        
+        if(ultP_u == ultP_v){
+            return; 
+        }
+        
+        if(size.get(ultP_u) < size.get(ultP_v)){
+            parent.set(ultP_u, ultP_v);
+            size.set(ultP_v, size.get(ultP_v) + size.get(ultP_u));
+        }
+        else{
+            parent.set(ultP_v, ultP_u);
+            size.set(ultP_u, size.get(ultP_v) + size.get(ultP_u));
+        }
+    }
+}
+
 class Solution {
     public int makeConnected(int n, int[][] connections) {
         
-        if(connections.length < n-1){
-            return -1;
+        DisjointSet ds = new DisjointSet(n);
+        int cntExtras = 0;
+        int m = connections.length;
+        for (int i = 0; i < m ; i++) {
+            int u = connections[i][0];
+            int v = connections[i][1];
+            
+            if (ds.findUltParent(u) == ds.findUltParent(v)) {
+                cntExtras++;
+            } else {
+                ds.unionBySize(u, v);
+            }
         }
-     
         
-        List<Integer> graph[] = new ArrayList[n];
-        for (int i = 0; i < n; i++){
-            graph[i] = new ArrayList<>();
+        int components = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent.get(i) == i){
+                components++;
+            } 
+        }
+        
+        int res = components - 1;
+        if (cntExtras >= res){
+            return res;
         } 
         
-        for (int[] c : connections) {
-            graph[c[0]].add(c[1]);
-            graph[c[1]].add(c[0]);
-        }
-        
-        boolean visited[] = new boolean[n];
-        int components = 0;
-        for(int i = 0; i < n; i++){
-            components += dfs(i, visited, graph);
-        }
-        
-        return components - 1;
-    }
-    
-    
-    public int dfs(int node, boolean visited[], List<Integer> graph[]){
-        if(visited[node]){
-            return 0;
-        }
-        
-        visited[node] = true;
-        for(int dest : graph[node]){
-            dfs(dest, visited, graph);
-        }
-        
-        return 1;
+        return -1;
     }
 }
