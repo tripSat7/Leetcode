ee22ab7bd7f403ff913044d5f4f17b0724a1bee1
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-21 14:53:54 -0400

    Time: 6 ms (71.68%), Space: 60.8 MB (76.20%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,73 +1,71 @@
-class DisjointSet{
-    List<Integer> size = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    
-    public DisjointSet(int n) {
-        for (int i = 0; i <= n; i++) {
-            parent.add(i);
-            size.add(1);
+class DisjointSet {
+    int parent[];
+    int size[];
+
+    DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
-    
-    public int findUltParent(int node){
-       
-        if(node == parent.get(node)){
-            return node;
+
+    public int find(int x) {
+        if (parent[x] == x) {
+            return x;
         }
-        
-        int ulp = findUltParent(parent.get(node));
-        parent.set(node, ulp);
-        return parent.get(node);
-    } 
-    
-    public void unionBySize(int u, int v){
-        int ultP_u = findUltParent(u);
-        int ultP_v = findUltParent(v);
-        
-        if(ultP_u == ultP_v){
-            return; 
+
+        return parent[x] = find(parent[x]);
+    }
+
+    public void unionBySize(int u, int v) {
+        int u_parent = find(u);
+        int v_parent = find(v);
+
+        if (u_parent == v_parent) {
+            return;
         }
-        
-        if(size.get(ultP_u) < size.get(ultP_v)){
-            parent.set(ultP_u, ultP_v);
-            size.set(ultP_v, size.get(ultP_v) + size.get(ultP_u));
-        }
-        else{
-            parent.set(ultP_v, ultP_u);
-            size.set(ultP_u, size.get(ultP_v) + size.get(ultP_u));
+
+        if (size[u_parent] < size[v_parent]) {
+            parent[u_parent] = v_parent;
+            size[v_parent] += size[u_parent];
+        } else {
+            parent[v_parent] = u_parent;
+            size[u_parent] += size[v_parent];
         }
     }
 }
 
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        
         DisjointSet ds = new DisjointSet(n);
         int cntExtras = 0;
         int m = connections.length;
-        for (int i = 0; i < m ; i++) {
+        for (int i = 0; i < m; i++) {
             int u = connections[i][0];
             int v = connections[i][1];
-            
-            if (ds.findUltParent(u) == ds.findUltParent(v)) {
+
+            if (ds.find(u) == ds.find(v)) {
                 cntExtras++;
             } else {
                 ds.unionBySize(u, v);
             }
         }
-        
+
         int components = 0;
         for (int i = 0; i < n; i++) {
-            if (ds.parent.get(i) == i){
+            if (ds.parent[i] == i) {
                 components++;
-            } 
+            }
         }
-        
+
         int res = components - 1;
-        if (cntExtras >= res){
+        if (cntExtras >= res) {
             return res;
-        } 
-        
+        }
+
         return -1;
     }
 }
