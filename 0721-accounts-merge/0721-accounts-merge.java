74c05b71a28e2d4ad5214e22655ddc56648eb611
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-08-27 20:01:06 -0400

    Time: 27 ms (89.21%), Space: 47.3 MB (87.57%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,87 +1,82 @@
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        
-        Map<String, Integer> mapMailNode = new HashMap<>();
+        List<List<String>> ans = new ArrayList<>();
         int n = accounts.size();
-        DisjointSet ds = new DisjointSet(n);
+        DSU ds = new DSU(n);
+    
+        Map<String, Integer> map = new HashMap<>();
         
-        for(int i = 0; i < n; i++){
-            for(int j = 1; j < accounts.get(i).size(); j++){
-                if(!mapMailNode.containsKey(accounts.get(i).get(j))){
-                    mapMailNode.put(accounts.get(i).get(j), i);
-                }else{
-                    ds.unionBySize(i, mapMailNode.get(accounts.get(i).get(j)));
+        for (int i = 0; i < n; i++) {
+            for(int j=1;j<accounts.get(i).size();j++){
+                String value = accounts.get(i).get(j);
+               
+                if(!map.containsKey(value)){
+                    map.put(value, i);
+                }
+                else {
+                    ds.unionBySize(i, map.get(value));
                 }
             }
         }
-        System.out.println(mapMailNode);
+
         ArrayList<String>[] mergedMail = new ArrayList[n];
-        for(int i = 0; i < n; i++){
-            mergedMail[i] = new ArrayList<String>();
-        }
-        
-        for(String key : mapMailNode.keySet()) {
-            String mail = key;
-            int node = ds.findParent(mapMailNode.get(key));
+        for (int i = 0; i < n; i++) mergedMail[i] = new ArrayList<String>();
+        for (Map.Entry<String, Integer> it : map.entrySet()) {
+            String mail = it.getKey();
+            int node = ds.findPar(it.getValue());
             mergedMail[node].add(mail);
         }
         
-        List<List<String>> res = new ArrayList<>();
-        
-        for(int i = 0; i < n; i++){
-            if(mergedMail[i].size() == 0){
-                continue;
-            }
-            
+        for (int i = 0; i < n; i++) {
+            if (mergedMail[i].size() == 0) continue;
             Collections.sort(mergedMail[i]);
             List<String> temp = new ArrayList<>();
             temp.add(accounts.get(i).get(0));
-            for(int j = 0; j < mergedMail[i].size(); j++){
-                temp.add(mergedMail[i].get(j));
+            for (String it : mergedMail[i]) {
+                temp.add(it);
             }
-            res.add(temp);
+            ans.add(temp);
         }
+        return ans;
+
         
-        return res;
     }
 }
 
-class DisjointSet{
-    int parent[];
-    int size[];
-    DisjointSet(int n){
-        parent = new int[n];
-        size = new int[n];
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-            size[i] = 1;
+class DSU {
+    List<Integer> size = new ArrayList<>();
+    List<Integer> parents = new ArrayList<>();
+    public DSU(int n){
+        for(int i=0;i<=n;i++){
+            size.add(1);
+            parents.add(i);
         }
     }
     
-    public int findParent(int n){
-        if(n == parent[n]){
-            return n;
+    public int findPar(int node){
+        if(node == parents.get(node)){
+            return node;
         }
-        
-        int par = findParent(parent[n]);
-        return par;
+        int ultp = findPar(parents.get(node));
+        parents.set(node, ultp);
+        return parents.get(node);
     }
     
     public void unionBySize(int u, int v){
-        int ulp_u = findParent(u);
-        int ulp_v = findParent(v);
-        
-        if(ulp_u == ulp_v){
+        int ultp_u = findPar(u);
+        int ultp_v = findPar(v);
+        if(ultp_u == ultp_v)
             return;
-        }
+        int size_u = size.get(ultp_u);
+        int size_v = size.get(ultp_v);
         
-        if(size[ulp_u] < size[ulp_v]){
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];  
-        }else{
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];  
+        if (size_u < size_v) {
+            parents.set(ultp_u, ultp_v);
+            size.set(ultp_v, size_u + size_v);
         }
-        
+        else {
+            parents.set(ultp_v, ultp_u);
+            size.set(ultp_u, size_u + size_v);
+        }
     }
 }
