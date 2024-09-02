026bdbdf60461de50232c72861964c207b0dbb07
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-09-02 17:23:49 -0400

    Time: 1 ms (100.00%), Space: 56.3 MB (16.85%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,34 +1,35 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         
-        int change[] = new int[3];
-        
-        for(int i : bills){
-            if(i == 5){
-                change[0]++;
-            }
-            else if(i == 10){
-                change[1]++;
-                if(change[0] == 0){
+        int five_dollars = 0, ten_dollars = 0;
+
+        for(int x : bills){
+            if(x == 5) {
+                five_dollars++;
+            } 
+            else if(x == 10){
+                if(five_dollars > 0){
+                    five_dollars--;
+                    ten_dollars++;
+                } 
+                else{
                     return false;
                 }
-                change[0]--;
-            }
+            } 
             else{
-                change[2]++;
-                if(change[1] >= 1 && change[0] >= 1){
-                    change[1]--;
-                    change[0]--;
-                }
-                else if(change[0] >= 3){
-                    change[0] -= 3;
-                }
+                if(five_dollars > 0 && ten_dollars > 0){
+                    five_dollars--;
+                    ten_dollars--;
+                } 
+                else if(five_dollars > 2){
+                    five_dollars -= 3;
+                } 
                 else{
                     return false;
                 }
             }
         }
-        
+
         return true;
     }
 }
