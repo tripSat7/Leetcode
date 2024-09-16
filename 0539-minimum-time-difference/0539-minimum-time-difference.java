ab779773d71277961bfa888996bc72f8dacd6ba8
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-09-16 17:28:57 -0400

    Time: 1 ms (100.00%), Space: 45.1 MB (80.15%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,20 +1,43 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
         
-        int res = Integer.MAX_VALUE;
-        int N = timePoints.size();
-        int[] c = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            String s = timePoints.get(i);
-            c[i] = Integer.parseInt(s.substring(0, 2)) * 60 + Integer.parseInt(s.substring(3, 5));
-        }
-        Arrays.sort(c);
-        for (int i = 1; i < N; i++) {
-            res = Math.min(res, c[i] - c[i - 1]);
+        if(timePoints.size() > 1440) {
+            return 0; 
         }
         
-        res = Math.min(res, c[0] + (24*60 - c[N - 1]));
-        return res;
+        boolean[] seen = new boolean[1440]; 
+        
+        for(String time : timePoints) {
+            int minutes = convertToMinutes(time);
+            if (seen[minutes]) {
+                return 0;
+            } 
+            seen[minutes] = true;
+        }
+        
+        int first = Integer.MAX_VALUE, prev = Integer.MAX_VALUE;
+        int minDiff = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < 1440; i++) {
+            if (seen[i]){
+                if (first == Integer.MAX_VALUE) {
+                    first = i;
+                }
+                else{
+                    minDiff = Math.min(minDiff, i - prev);
+                }
+                prev = i;
+            }
+        }
+        
+
+        minDiff = Math.min(minDiff, 1440 - prev + first);
+        
+        return minDiff;
+    }
+    
+    private int convertToMinutes(String time) {
+        return ((time.charAt(0) - '0') * 10 + (time.charAt(1) - '0')) * 60 
+             + (time.charAt(3) - '0') * 10 + (time.charAt(4) - '0');
     }
 }
