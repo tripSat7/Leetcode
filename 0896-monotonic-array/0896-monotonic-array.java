5bdf1c7fe8ec4be3f0ba1f8f3695cc7692bb5ef0
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-12-21 01:49:30 -0500

    Time: 2 ms (65.10%), Space: 59.2 MB (66.54%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,15 +1,14 @@
 class Solution {
     private boolean increasing(int[] nums){
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i + 1] < nums[i])
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] > nums[i])
                 return false;
         }
         return true;
     }
-    
     private boolean decreasing(int[] nums){
-        for(int i = 0; i < nums.length - 1; i++){
-            if(nums[i + 1] > nums[i])
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] < nums[i])
                 return false;
         }
         return true;
