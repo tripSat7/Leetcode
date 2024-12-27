87fd459bc99cab496c081e926240bd0989f1362f
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-12-27 11:03:01 -0500

    Time: 2 ms (89.3%), Space: 55.2 MB (76.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,12 +3,12 @@ class Solution {
         int res[] = new int[nums.length];
         int postfix=1;
         res[0]=1;
-        for(int i=1;i<nums.length;i++){
-            res[i]=res[i-1]*nums[i-1];
+        for(int i = 1; i < nums.length; i++){
+            res[i] = res[i-1] * nums[i-1];
         }
-        for(int i=nums.length-2;i>=0;i--){
-            postfix=postfix*nums[i+1];
-            res[i] = postfix*res[i];
+        for(int i = nums.length-2; i >= 0; i--){
+            postfix = postfix * nums[i+1];
+            res[i] = postfix * res[i];
         }
         
         return res;
