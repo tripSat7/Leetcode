5ed9bc929fa26a24189a21c0201c3238a9e035af
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-12-28 14:22:59 -0500

    Time: 12 ms (90.62%), Space: 49.7 MB (7.69%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.java
+++ 0554-brick-wall/0554-brick-wall.java
@@ -1,22 +1,21 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        int max = 0;
 
         for(List<Integer> row : wall){
             int prefix = 0;
-            for(int i : row){
-                prefix += i;
-                map.put(prefix, map.getOrDefault(prefix,0)+1);
-                max = Math.max(prefix, max);
+            for(int i = 0; i < row.size()-1; i++){
+
+                prefix += row.get(i);
+                map.put(prefix, map.getOrDefault(prefix,0) + 1);
             }
         }
 
-        if(map.size() == 1){
-            return wall.size();
+        int max = 0;
+        for(int num : map.values()){
+            max = Math.max(max, num);
         }
-        map.remove(max);
-        
-        return wall.size() - Collections.max( map.values());
+
+        return  wall.size() - max;
     }
 }
