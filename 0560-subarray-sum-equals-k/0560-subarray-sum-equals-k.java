0068249faf85f7ca6958e647de05a0931d43dd96
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-12-29 12:31:08 -0500

    Time: 24 ms (73.27%), Space: 46.3 MB (73.87%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,16 @@
-class Solution {
+public class Solution {
     public int subarraySum(int[] nums, int k) {
-        int length = nums.length;
-        int[] prefix = new int[length + 1];
-        prefix[0] = 0;
+        int res = 0, curSum = 0;
+        Map<Integer, Integer> prefixSums = new HashMap<>();
+        prefixSums.put(0, 1);
 
-        int sum = 0;
-        for (int i = 0; i < length; i++) {
-            sum += nums[i];
-            prefix[i + 1] = sum;
+        for (int num : nums) {
+            curSum += num;
+            int diff = curSum - k;
+            res += prefixSums.getOrDefault(diff, 0);
+            prefixSums.put(curSum, prefixSums.getOrDefault(curSum, 0) + 1);
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i <= length; i++) {
-            count += map.getOrDefault(prefix[i] - k, 0);
-            map.put(prefix[i], map.getOrDefault(prefix[i], 0) + 1);
-        }
-
-        return count;
+        return res;
     }
 }
