900832a8cb118bca59e7546b217b870fe8f9af32
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-12-31 06:37:36 -0500

    Time: 54 ms (29.38%), Space: 98.3 MB (27.97%) - LeetHub

--- 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
+++ 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.java
@@ -1,16 +1,11 @@
 public  class Solution {
     public long interchangeableRectangles(int[][] rectangles) {
         HashMap<Double, Integer> count = new HashMap<>();
+        long res = 0;
         for (int[] rect : rectangles) {
             double ratio = (double) rect[0] / rect[1];
+            res += count.getOrDefault(ratio, 0);
             count.put(ratio, count.getOrDefault(ratio, 0) + 1);
-        }
-        
-        long res = 0;
-        for (int c : count.values()) {
-            if (c > 1) {
-                res += (c * 1L * (c - 1)) / 2;
-            }
         }
         return res;
     }
