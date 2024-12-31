6da5a7918954cf3f096a09dc74bf76d245d8657e
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2024-12-31 13:32:08 -0500

    Time: 6 ms (41.61%), Space: 57.6 MB (51.55%) - LeetHub

---
+++ 2145-grid-game/2145-grid-game.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long gridGame(int[][] grid) {
+        int n = grid[0].length;
+
+        // Prefix sums for both rows
+        long[] preRow1 = new long[n];
+        long[] preRow2 = new long[n];
+
+        preRow1[0] = grid[0][0];
+        preRow2[0] = grid[1][0];
+        
+        for (int i = 1; i < n; i++) {
+            preRow1[i] = preRow1[i - 1] + grid[0][i];
+            preRow2[i] = preRow2[i - 1] + grid[1][i];
+        }
+
+        long result = Long.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            long topPoints = preRow1[n - 1] - preRow1[i];
+            long bottomPoints = (i > 0) ? preRow2[i - 1] : 0;
+
+            long secondRobotPoints = Math.max(topPoints, bottomPoints);
+
+            result = Math.min(result, secondRobotPoints);
+        }
+
+        return result;
+    }
+}
