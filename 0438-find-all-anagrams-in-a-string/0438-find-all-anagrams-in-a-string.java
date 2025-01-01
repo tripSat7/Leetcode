f948c86ee435b13f69f51665abfffbcd7fc2c0ed
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-01-01 02:45:58 -0500

    Time: 9 ms (91.43%), Space: 44.9 MB (64.1%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,50 +1,30 @@
-class Solution {
+public class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int n = p.length();
+        if (p.length() > s.length()) return new ArrayList<>();
+        
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+        
+        for (char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+        for (int i = 0; i < p.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+        }
+        
         List<Integer> res = new ArrayList<>();
-        int freq[] = new int[26];
-        int l = 0, r = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(char ch : p.toCharArray()){
-            map.put(ch-'a', map.getOrDefault(ch-'a',0)+1);
-        }
-
-        while(r < s.length()){
-            freq[s.charAt(r) - 'a']++;
-
-            if(r - l == n - 1){
-                boolean isAna = true;
-                
-                for(int i = 0; i < 26; i++){
-                    //System.out.println(r+"||"+i+"::"+freq[i]);
-                    if(map.containsKey(i) && freq[i] != map.get(i)){
-                        isAna = false;
-                        break;
-                    }
-                }
-                if(isAna){
-                    res.add(l);
-                }
+        if (Arrays.equals(pCount, sCount)) res.add(0);
+        
+        int l = 0;
+        for (int r = p.length(); r < s.length(); r++) {
+            sCount[s.charAt(r) - 'a']++;
+            sCount[s.charAt(l) - 'a']--;
+            l++;
+            if (Arrays.equals(pCount, sCount)) {
+                res.add(l);
             }
-            else if(r - l > n-1){
-                freq[s.charAt(l) - 'a']--;
-                l++;
-                boolean isAna = true;
-                for(int i = 0; i < 26; i++){
-                    if(map.containsKey(i) && freq[i] != map.get(i)){
-                        isAna = false;
-                        break;
-                    }
-                }
-                if(isAna){
-                    res.add(l);
-                }
-            }
-
-            r++;
         }
-
+        
         return res;
     }
 }
