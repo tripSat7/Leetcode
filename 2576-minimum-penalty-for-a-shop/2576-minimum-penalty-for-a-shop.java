236c31b63057459492a036ea5bb3ae635177478c
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-01-09 04:20:21 -0500

    Time: 12 ms (61.7%), Space: 45.2 MB (68.7%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.java
@@ -1,22 +1,31 @@
 public class Solution {
     public int bestClosingTime(String customers) {
-        int cntY = 0;
-        for (char c : customers.toCharArray()) {
-            if (c == 'Y') cntY++;
+        int n = customers.length();
+        int cnt = 0;
+
+        int[] prefixN = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixN[i] = cnt;
+            if (customers.charAt(i) == 'N') {
+                cnt++;
+            }
+        }
+        prefixN[n] = cnt;
+
+        int[] suffixY = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            suffixY[i] = suffixY[i + 1];
+            if (customers.charAt(i) == 'Y') {
+                suffixY[i]++;
+            }
         }
 
-        int minPenalty = cntY, res = 0, cntN = 0;
-        for (int i = 0; i < customers.length(); i++) {
-            if (customers.charAt(i) == 'Y') {
-                cntY--;
-            } else {
-                cntN++;
-            }
-
-            int penalty = cntN + cntY;
+        int res = n, minPenalty = n;
+        for (int i = 0; i <= n; i++) {
+            int penalty = prefixN[i] + suffixY[i];
             if (penalty < minPenalty) {
-                res = i + 1;
                 minPenalty = penalty;
+                res = i;
             }
         }
 
