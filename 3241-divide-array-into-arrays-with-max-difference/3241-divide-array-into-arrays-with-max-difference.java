e2c815ea446fe4526f8672ca803d33415da7d194
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-01-12 15:16:50 -0500

    Time: 24 ms (41.27%), Space: 61.8 MB (26.51%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.java
@@ -3,16 +3,13 @@ class Solution {
         Arrays.sort(nums);
         int res[][] = new int[nums.length/3][3];
 
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i+=3){
             int row = i/3;
             if(!(nums[i+2] - nums[i] <= k)){
                 return new int[][]{};
             }
 
-            for(int j = 0; j < 3; j++){
-                res[row][j] = nums[i++];
-            }
-            i--;
+            res[row] = new int[] {nums[i], nums[i+1], nums[i+2]};
         }
 
         return res;
