1b7fc8ab62b564fb08953d6dbdfa53272e2b6d33
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-02-04 17:46:58 -0500

    Time: 8 ms (77.83%), Space: 45.3 MB (14.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,25 +1,40 @@
 class Solution {
+    private int totalSum;
+    private int[][] memo;
+
     public int findTargetSumWays(int[] nums, int target) {
+        totalSum = 0;
+        for(int num : nums) {
+            totalSum += num;
+        }
+        int n = nums.length;
+        memo = new int[n][2 * totalSum + 1];
+        for(int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
         return helper(nums, target, nums.length - 1, 0);
     }
 
-    public int helper(int[] nums, int target, int i, int sum){
-
-        if(i == 0){
-            if(target == sum && nums[0] == 0){
+    private int helper(int[] nums, int target, int i, int sum) {
+        if(i == 0) {
+            if(sum == target && nums[0] == 0) {
                 return 2;
             }
-
-            if(target == sum + nums[0] || target == sum - nums[0]){
+            if(sum + nums[0] == target || sum - nums[0] == target) {
                 return 1;
             }
-
             return 0;
         }
 
-        int notTake = helper(nums, target, i - 1, sum - nums[i]);
-        int take = helper(nums, target, i - 1, sum + nums[i]);
+        int shiftedSum = sum + totalSum;
+        if (memo[i][shiftedSum] != -1) {
+            return memo[i][shiftedSum];
+        }
 
-        return take + notTake;
+        int subtract = helper(nums, target, i - 1, sum - nums[i]);
+        int add = helper(nums, target, i - 1, sum + nums[i]);
+
+        memo[i][shiftedSum] = subtract + add;
+        return memo[i][shiftedSum];
     }
 }
