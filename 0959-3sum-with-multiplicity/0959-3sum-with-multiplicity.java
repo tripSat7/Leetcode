a6f37393bb5210d831e78a532726055c4d904eba
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-02-12 12:02:19 -0500

    Time: 34 ms (45.45%), Space: 44.5 MB (41.16%) - LeetHub

--- 0959-3sum-with-multiplicity/0959-3sum-with-multiplicity.java
+++ 0959-3sum-with-multiplicity/0959-3sum-with-multiplicity.java
@@ -1,17 +1,41 @@
 class Solution {
-    public int threeSumMulti(int[] A, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        int res = 0;
-        int mod = 1000000007;
-        for (int i = 0; i < A.length; i++) {
-            res = (res + map.getOrDefault(target - A[i], 0)) % mod;
-            
-            for (int j = 0; j < i; j++) {
-                int temp = A[i] + A[j];
-                map.put(temp, map.getOrDefault(temp, 0) + 1);
+    public int threeSumMulti(int[] nums, int target) {
+        Arrays.sort(nums);
+        long output = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int sum = target - nums[i];
+            int j = i + 1, k = nums.length - 1;
+
+            while (j < k) {
+                if(nums[j] + nums[k] < sum){
+                    j++;
+                } 
+                else if(nums[j] + nums[k] > sum){
+                    k--;
+                } 
+                else {
+                    int count1 = 1, count2 = 1;
+                    if (nums[j] == nums[k]) {
+                        int len = k - j + 1;
+                        output += (len * (len - 1) / 2);
+                        break;
+                    }
+
+                    while (j < k && nums[j] == nums[j + 1]){
+                        count1++; j++; 
+                    }
+
+                    while (k > j && nums[k] == nums[k - 1]) {
+                        count2++; k--; 
+                    }
+
+                    output += count1 * count2;
+                    j++; k--;
+                }
             }
         }
-        return res;
+
+        return (int) (output % 1000000007);
     }
 }
