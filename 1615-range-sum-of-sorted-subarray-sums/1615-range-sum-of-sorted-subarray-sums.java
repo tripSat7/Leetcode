29dae42977fc70a0cef866eeff010849f60bbf90
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-02-17 12:31:37 -0500

    Time: 64 ms (84.73%), Space: 46.3 MB (68.16%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.java
@@ -1,21 +1,27 @@
 class Solution {
-
     public int rangeSum(int[] nums, int n, int left, int right) {
-        ArrayList<Integer> storeSubarray = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                storeSubarray.add(sum);
+        int sum = 0;
+        int k = 0;
+
+        int[] arr = new int[(n*(n+1))/2];
+
+        for(int i = 0; i < n; i++){
+            sum=0;
+            for(int j = i; j < n; j++)
+            {
+                sum = sum + nums[j];
+                arr[k] = sum;
+                k++;
             }
         }
-
-        Collections.sort(storeSubarray);
-
-        int rangeSum = 0, mod = (int) 1e9 + 7;
-        for (int i = left - 1; i <= right - 1; i++) {
-            rangeSum = (rangeSum + storeSubarray.get(i)) % mod;
+        
+        int mod = 1000000007;
+        Arrays.sort(arr);
+        int ans = 0;
+        for(int i = left-1; i < right; i++)
+        {
+            ans = (ans + arr[i]) % mod;
         }
-        return rangeSum;
+        return ans;
     }
 }
