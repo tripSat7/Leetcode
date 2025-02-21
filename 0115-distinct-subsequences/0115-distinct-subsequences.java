732f01ae049ddadf3770c68c96381bd31e359434
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-02-21 12:12:11 -0500

    Time: 18 ms (79.01%), Space: 51.5 MB (23.49%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,38 +1,30 @@
 class Solution {
     public int numDistinct(String s, String t) {
 
-        int lt = s.length();
-        int ls = t.length();
+        int n = s.length();
+        int m = t.length();
 
-        int dp[][] = new int[lt][ls];
-        for (int rows[] : dp){
-            Arrays.fill(rows, -1);
-        }
-            
-        return countUtil(s, t, lt - 1, ls - 1, dp);
-    }
+        int dp[][] = new int[n + 1][m + 1];
 
-    public int countUtil(String s1, String s2, int ind1, int ind2, int[][] dp) {
-        if (ind2 < 0){
-            return 1;
-        }
-            
-        if (ind1 < 0){
-            return 0;
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 1;
         }
 
-        if (dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
+        for (int i = 1; i < m + 1; i++) {
+            dp[0][i] = 0;
         }
-            
-        if (s1.charAt(ind1) == s2.charAt(ind2)) {
-            int leaveOne = countUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
-            int stay = countUtil(s1, s2, ind1 - 1, ind2, dp);
 
-            return dp[ind1][ind2] = (leaveOne + stay);
-        } 
-        else {
-            return dp[ind1][ind2] = countUtil(s1, s2, ind1 - 1, ind2, dp);
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]);
+                } 
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
+
+        return dp[n][m];
     }
 }
