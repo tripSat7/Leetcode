912c0b47c0d0d1d1a717c8eb080c4004418d2681
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-02-22 03:14:25 -0500

    Time: 2 ms (100%), Space: 45.3 MB (18.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,28 +1,44 @@
 class Solution {
-    public int minDistance(String S1, String S2) {
-        int n = S1.length();
-        int m = S2.length();
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
 
-        int[][] dp = new int[n + 1][m + 1];
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = i;
+        int[][] dp = new int[n][m];
+        for (int row[] : dp){
+            Arrays.fill(row, -1);
         }
-        for (int j = 0; j <= m; j++) {
-            dp[0][j] = j;
-        }
+            
+        return editDistanceUtil(word1, word2, n - 1, m - 1, dp);
+    }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (S1.charAt(i - 1) == S2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } 
-                else {
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
-                }
-            }
+    int editDistanceUtil(String s1, String s2, int i, int j, int[][] dp) {
+        
+        if (i < 0){
+            return j + 1;
         }
+            
+        if (j < 0){
+            return i + 1;
+        }
+            
+        if (dp[i][j] != -1){
+            return dp[i][j];
+        }
+            
 
-        return dp[n][m];
+        if (s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = editDistanceUtil(s1, s2, i - 1, j - 1, dp);
+        }
+            
+
+        // Minimum of three choices:
+        // 1. Replace the character in S1 with the character in S2.
+        // 2. Delete the character in S1.
+        // 3. Insert the character from S2 into S1.
+        else{
+            return dp[i][j] = 1 + Math.min(editDistanceUtil(s1, s2, i - 1, j - 1, dp),
+                    Math.min(editDistanceUtil(s1, s2, i - 1, j, dp), editDistanceUtil(s1, s2, i, j - 1, dp)));
+        }
+            
     }
 }
