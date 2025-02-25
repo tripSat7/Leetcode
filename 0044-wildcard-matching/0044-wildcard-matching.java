72c94946cf9c448441013b0aee9dc159b488253e
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-02-25 14:22:37 -0500

    Time: 24 ms (32.6%), Space: 44.8 MB (70.2%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -3,48 +3,36 @@ class Solution {
         int n = s.length();
         int m = p.length();
 
-        int dp[][] = new int[n][m]; 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        boolean dp[][] = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = isAllStars(p, j);
         }
 
-        return wildcardMatchingUtil(s, p, n - 1, m - 1, dp) == 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1]; 
+                } 
+                else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1]; 
+                } 
+                else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 
     public boolean isAllStars(String S, int i) {
-        for (int j = 0; j <= i; j++) {
+        for (int j = 0; j < i; j++) { // Fixed loop condition
             if (S.charAt(j) != '*') {
                 return false;
             }
         }
         return true;
-    }
-
-    public int wildcardMatchingUtil(String S1, String S2, int i, int j, int[][] dp) {
-        if(i < 0 && j < 0){
-            return 1;
-        } 
-
-        if(i < 0 && j >= 0){
-            return isAllStars(S2, j) ? 1 : 0; 
-        } 
-
-        if(j < 0 && i >= 0){
-            return 0;
-        }  
-
-        if (dp[i][j] != -1){
-            return dp[i][j];
-        } 
-
-        if(S1.charAt(i) == S2.charAt(j) || S2.charAt(j) == '?'){
-            return dp[i][j] = wildcardMatchingUtil(S1, S2, i - 1, j - 1, dp);
-        } 
-        else if(S2.charAt(j) == '*'){
-            return dp[i][j] = (wildcardMatchingUtil(S1, S2, i, j - 1, dp) == 1 || wildcardMatchingUtil(S1, S2, i - 1, j, dp) == 1) ? 1 : 0;
-        } 
-        else {
-            return 0;
-        }
     }
 }
