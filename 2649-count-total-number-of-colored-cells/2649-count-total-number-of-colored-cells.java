fe88506c64bf6b6f1039b78ddc2bd7ab4f015c96
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-03-05 17:49:40 -0500

    Time: 4 ms (13.28%), Space: 40.6 MB (45.64%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,5 +1,9 @@
 class Solution {
     public long coloredCells(int n) {
-        return 1 + (long) n * (n - 1) * 2;
+        long res = 1;
+        for (int i = 1; i <= n; i++) {
+            res += 4 * (i - 1);
+        }
+        return res;
     }
 }
