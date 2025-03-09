d63baaf0d544a4f48c8b3fdbede9fe98ae50543b
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-03-09 04:20:43 -0400

    Time: 1 ms (96.18%), Space: 43.2 MB (50.88%) - LeetHub

---
+++ 3463-alternating-groups-i/3463-alternating-groups-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors) {
+        int n = colors.length;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(colors[(i) % n] != colors[(i+1) % n] && colors[(i+1) % n] != colors[(i+2) % n]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
