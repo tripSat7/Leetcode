4c285b36047ebf697baf2767c66bcc39a94133c0
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-03-11 16:40:46 -0400

    Time: 3 ms (89.43%), Space: 43 MB (53.3%) - LeetHub

---
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countOfSubstrings(String word, int k) {
+        return atLeastK(word, k) - atLeastK(word, k + 1);
+    }
+
+    private int atLeastK(String word, int k) {
+        int ans = 0;
+        int start = 0;
+        int end = 0;
+        HashMap<Character, Integer> vowelCount = new HashMap<>();
+        int consonantCount = 0;
+
+        while(end < word.length()) {
+            char c = word.charAt(end);
+            if(this.isVowel(c)) {
+                vowelCount.put(c, vowelCount.getOrDefault(c, 0) + 1);
+            } else {
+                consonantCount++;
+            }
+            while(vowelCount.size() == 5 && consonantCount >= k) {
+                ans += word.length() - end;
+                char startC = word.charAt(start);
+                if(isVowel(startC)) {
+                    vowelCount.put(startC, vowelCount.get(startC) - 1);
+                    if(vowelCount.get(startC) == 0) {
+                        vowelCount.remove(startC);
+                    }
+                } else {
+                    consonantCount--;
+                }
+                start++;
+            }
+            end++;
+        }
+        return ans;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+} 
