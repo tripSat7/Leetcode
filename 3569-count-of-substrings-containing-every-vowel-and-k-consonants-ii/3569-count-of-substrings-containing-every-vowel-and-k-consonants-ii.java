f3b8bcf6b2859fd9f898f49375ac36472b32bde3
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-03-11 16:44:41 -0400

    Time: 280 ms (36.33%), Space: 45.7 MB (97.6%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -4,48 +4,38 @@ class Solution {
     }
 
     private long atLeastK(String word, int k) {
-        long numValidSubstrings = 0;
+        long ans = 0;
         int start = 0;
         int end = 0;
         HashMap<Character, Integer> vowelCount = new HashMap<>();
         int consonantCount = 0;
 
-        while (end < word.length()) {
-            char newLetter = word.charAt(end);
-
-            if (isVowel(newLetter)) {
-                vowelCount.put(
-                    newLetter,
-                    vowelCount.getOrDefault(newLetter, 0) + 1
-                );
+        while(end < word.length()) {
+            char c = word.charAt(end);
+            if(this.isVowel(c)) {
+                vowelCount.put(c, vowelCount.getOrDefault(c, 0) + 1);
             } else {
                 consonantCount++;
             }
-
-            while (vowelCount.size() == 5 && consonantCount >= k) {
-                numValidSubstrings += word.length() - end;
-                char startLetter = word.charAt(start);
-                if (isVowel(startLetter)) {
-                    vowelCount.put(
-                        startLetter,
-                        vowelCount.get(startLetter) - 1
-                    );
-                    if (vowelCount.get(startLetter) == 0) {
-                        vowelCount.remove(startLetter);
+            while(vowelCount.size() == 5 && consonantCount >= k) {
+                ans += word.length() - end;
+                char startC = word.charAt(start);
+                if(isVowel(startC)) {
+                    vowelCount.put(startC, vowelCount.get(startC) - 1);
+                    if(vowelCount.get(startC) == 0) {
+                        vowelCount.remove(startC);
                     }
                 } else {
                     consonantCount--;
                 }
                 start++;
             }
-
             end++;
         }
-
-        return numValidSubstrings;
+        return ans;
     }
 
     private boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
-}
+} 
