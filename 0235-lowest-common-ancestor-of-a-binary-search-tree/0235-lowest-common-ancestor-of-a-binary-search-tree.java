7f58b185e620bd50d2d458d103209c1ffb9c39f2
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-04-03 00:15:54 -0400

    Time: 13 ms (1.47%), Space: 45.6 MB (5.13%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,21 +10,52 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-        if(root == null){
-            return null;
+        System.out.println(root.val +"||"+ p.val+"||"+ q.val);
+        if(q.val < p.val){
+            return lowestCommonAncestor(root, q, p);
         }
+
         
-        if(root == p || root == q){
+        if(root.val > p.val && root.val < q.val){
             return root;
         }
-        
-        TreeNode left =  lowestCommonAncestor(root.left, p, q);
-        TreeNode right =  lowestCommonAncestor(root.right, p, q);
-        
-        if(left != null && right != null){
+
+        if(p.val == root.val){
+            return p;
+        }
+
+        if(q.val == root.val){
+            return q;
+        }
+
+        if(p.val < root.val){
+            return helper(root.left, p, q);
+        }
+        else{
+            return helper(root.right, p, q);
+        }
+
+    }
+
+    public TreeNode helper(TreeNode root, TreeNode p, TreeNode q){
+        System.out.println(root.val);
+        if(root.val > p.val && root.val < q.val){
             return root;
         }
-        return left!=null ? left: right;
+
+        if(p.val == root.val){
+            return p;
+        }
+
+        if(q.val == root.val){
+            return q;
+        }
+
+        if(p.val < root.val){
+            return helper(root.left, p, q);
+        }
+        else{
+            return helper(root.right, p, q);
+        }
     }
 }
