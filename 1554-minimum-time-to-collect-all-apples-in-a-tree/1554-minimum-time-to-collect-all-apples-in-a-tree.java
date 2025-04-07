cbc80ff662889705349350bf1381dc1f2088c0ee
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-04-07 12:27:24 -0400

    Time: 17 ms (92.48%), Space: 76.4 MB (55.06%) - LeetHub

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/1554-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/1554-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,20 +1,22 @@
 public class Solution {
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
-        List<Integer>[] adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
-        for (int[] edge : edges) {
-            adj[edge[0]].add(edge[1]);
-            adj[edge[1]].add(edge[0]);
+        for(int i = 0; i < edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
 
         return dfs(0, -1, adj, hasApple);
     }
 
-    private int dfs(int cur, int parent, List<Integer>[] adj, List<Boolean> hasApple) {
+    private int dfs(int cur, int parent, List<List<Integer>> adj, List<Boolean> hasApple) {
         int time = 0;
-        for(int child : adj[cur]) {
+        for(int child : adj.get(cur)) {
             if(child == parent){
                 continue;
             } 
