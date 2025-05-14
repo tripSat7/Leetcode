ffd5970dd8bde96e2fd5e3aff84fc1bf991b69e2
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-05-14 19:39:17 -0400

    Time: 19 ms (18.9%), Space: 56.3 MB (32.65%) - LeetHub

---
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        if(p == 0){
+            return 0;
+        } 
+        
+        Arrays.sort(nums);
+        int left = 0, right = nums[nums.length - 1] - nums[0];
+        int result = right;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isValid(nums, mid, p)) {
+                result = mid;
+                right = mid - 1;
+            } 
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return result;
+    }
+    
+    private boolean isValid(int[] nums, int threshold, int p) {
+        int i = 0, count = 0;
+        while (i < nums.length - 1) {
+            if (Math.abs(nums[i] - nums[i + 1]) <= threshold) {
+                count++;
+                i += 2;
+            } else {
+                i++;
+            }
+            
+            if (count == p){
+                return true;
+            } 
+        }
+        return false;
+    }
+}
