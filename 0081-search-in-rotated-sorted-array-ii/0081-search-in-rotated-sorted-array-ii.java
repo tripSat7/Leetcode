9393de03a29315b21e841b5b20f02fcec5d5254e
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-05-16 18:38:11 -0400

    Time: 0 ms (100%), Space: 43.7 MB (17.7%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -7,16 +7,25 @@ class Solution {
             if (nums[mid] == target) return true;
 
             if (nums[low] == nums[mid]) {
-                low++;
+                low++;;
                 continue;
             }
 
             if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
-                else low = mid + 1;
-            } else {
-                if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
-                else high = mid - 1;
+                if (nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                } 
+            } 
+            else {
+                if (nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;  
+                }
+                else {
+                    high = mid - 1;
+                }
             }
         }
         return false;
