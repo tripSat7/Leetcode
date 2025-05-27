bc51c96da3c391dd6fad89eb8a0caa7666908e28
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-05-27 01:20:00 -0400

    Time: 2 ms (70.83%), Space: 44.6 MB (60.41%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid[i][j] == 1) {
-                    maxArea = java.lang.Math.max(maxArea, markVisited(i, j, m, n, grid));
+                    maxArea = Math.max(maxArea, markVisited(i, j, m, n, grid));
                 }
             }
         }
