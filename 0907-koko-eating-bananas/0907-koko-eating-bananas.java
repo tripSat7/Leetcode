6aadc72eb54ffe8fa7e6532d75dc99c117c5e3a8
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-14 19:08:18 -0400

    Time: 6 ms (99.3%), Space: 45.3 MB (47.05%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,18 +1,29 @@
+/**
+ Time Complexity: O(N * logM)
+ N = number of piles
+ M = max number of bananas in any pile
+ Space Complexity: O(1)
+ */
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int max = 0;
+        
         for (int p : piles){
             max = Math.max(max, p);
         } 
+        // left and right boundaries 
         int i = 1;
         int j = max;
+
+        //O(log M)
         while (i < j) {
             int mid = i + (j-i)/2;
+            // hour stands for the total hour Koko spends.
             int hour = getHour(piles, mid);
-            if (hour > h){
+            if (hour > h){//eating rate too slow, so increase rate
                 i = mid + 1;
             } 
-            else{
+            else{//eating rate too fast, so decrease rate
                 j = mid;
             } 
         }
@@ -21,9 +32,13 @@ class Solution {
 
     private int getHour(int[] piles, int k) {
         int res = 0;
+        
+        //O(n)
         for(int p : piles){
-            res += (p + k - 1)/k;
+            res += (p + k - 1)/k; 
+            //using (numerator + denom - 1)/denom to get the ceil value
         } 
+        
         return res;
     }
 }
