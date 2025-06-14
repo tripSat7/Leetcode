c1ba9f6025231438f2160b033cb7a084513c932c
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-14 18:26:48 -0400

    Time: 16 ms (21.06%), Space: 48.7 MB (52.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,35 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // O(1) time
+        if (k == nums.length) {
+            return nums;
+        }
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int ans[] = new int[k];
-
-        for(int i : nums){
-            map.put(i, map.getOrDefault(i,0)+1);
+        // 1. Build hash map: character and how often it appears
+        // O(N) time
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+          count.put(n, count.getOrDefault(n, 0) + 1);
         }
 
-        List<Integer> bucket[] = new ArrayList[nums.length+1];
-        
-        for(int i : map.keySet()){
-            int freq = map.get(i);
-            if(bucket[freq] == null){
-                bucket[freq] =  new ArrayList<>();
-            }
-            bucket[freq].add(i);
+        // initialize min heap : 'the less frequent element first'
+        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
+
+        // 2. Keep k top frequent elements in the heap
+        // O(N log k) < O(N log N) time
+        for (int n: count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll(); 
+            }   
         }
-        
-        List<Integer> res = new ArrayList<>();
-        for(int i = bucket.length - 1; i > 0  && k > 0; i--){
-            if(bucket[i] != null){
-                res.addAll(bucket[i]);
-                k = k - bucket[i].size();
-            }
+
+        // 3. Build an output array
+        // O(k log k) time
+        int[] top = new int[k];
+        for(int i = k - 1; i >= 0; --i) {
+            top[i] = heap.poll();
         }
-        
-        for(int i = 0; i < res.size(); i++){
-            ans[i] = res.get(i);
-        }
-        
-        return ans;
+        return top;
     }
 }
