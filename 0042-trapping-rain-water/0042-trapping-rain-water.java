53a255a305a2b18274072770bcfc3eab4e0192c3
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-14 12:46:19 -0400

    Time: 1 ms (62.22%), Space: 46.7 MB (20.67%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,22 @@
 class Solution {
-    public int trap(int[] arr) {
-        int n = arr.length;
-        int left[] = new int[n];
-		int right[] = new int[n];
-
-		int water = 0;
-
-		left[0] = arr[0];
-		for (int i = 1; i < n; i++){
-            left[i] = Math.max(left[i - 1], arr[i]);   
+    public int trap(int[] height) {
+        // time : O(n)
+        // space : O(1)
+        if (height.length==0) return 0; 
+        int left = 0, right = height.length-1; 
+        int leftMax=0, rightMax=0; 
+        int ans = 0; 
+        while (left < right) {
+            if (height[left] > leftMax) leftMax = height[left]; 
+            if (height[right] > rightMax) rightMax = height[right];
+            if (leftMax < rightMax) {
+                ans += Math.max(0, leftMax - height[left]); 
+                left++; 
+            } else {
+                ans += Math.max(0, rightMax - height[right]); 
+                right--; 
+            }
         }
-
-		right[n - 1] = arr[n - 1];
-		for (int i = n - 2; i >= 0; i--){
-            right[i] = Math.max(right[i + 1], arr[i]);    
-        }
-		
-		for (int i = 0; i < n; i++){
-            water += Math.min(left[i], right[i]) - arr[i];
-        }
-			
-		return water;
+        return ans; 
     }
 }
-
-//Time complexity: O(n)
-//Space : O(n) extra space
