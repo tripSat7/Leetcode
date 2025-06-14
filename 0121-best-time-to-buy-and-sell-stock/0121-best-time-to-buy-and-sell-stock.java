4427cd4bd9ee017d67487ea4b374ea5b6fd9ad64
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-14 10:27:26 -0400

    Time: 2 ms (74.97%), Space: 62.2 MB (7.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,18 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int low= Integer.MAX_VALUE;
-        int res = 0;
-        int curProf = 0;
-        
-        for(int i: prices){
-            if(i<low){
-                low = i;
-            }
-            
-            curProf = i - low;
-            if(curProf>res){
-                res = curProf;
-            }
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice){
+                minprice = prices[i];
+            } 
+            else if(prices[i] - minprice > maxprofit){
+                maxprofit = prices[i] - minprice;
+            } 
         }
-        return res;
+        return maxprofit;
     }
 }
+
+// Time complexity: O(n)
+// space complexity : O(1)
