61b747206902ac62f7bb1a729e1632e8bcdcc426
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-14 11:33:01 -0400

    Time: 49 ms (89.04%), Space: 58.9 MB (87.2%) - LeetHub

---
+++ 3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues/3894-maximize-ysum-by-picking-a-triplet-of-distinct-xvalues.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxSumDistinctTriplet(int[] x, int[] y) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < x.length; i++){
+            map.put(x[i], Math.max(y[i], map.getOrDefault(x[i], 0) ) );
+        }
+
+        if(map.size() < 3){
+            return -1;
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int val : map.values()) {
+            maxHeap.add(val);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += maxHeap.poll();
+        }
+
+        return sum;
+    }
+}
