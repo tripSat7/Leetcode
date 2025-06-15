b158bfe1b9c500af7a47d7f99211f3c531d79b4d
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-15 00:41:43 -0400

    Time: 0 ms (100%), Space: 42.7 MB (97.36%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,3 +1,5 @@
+// Time Complexity: O(2^2n) in worst case
+// Space Complexity: O(n) 
 class Solution {
     
     public List<String> generateParenthesis(int n) {
@@ -7,17 +9,20 @@ class Solution {
     }
     
     public void generation(List<String> res, StringBuilder s, int left, int right){
+        // Base case: no more brackets to add
         if(left == 0 && right == 0){
             res.add(s.toString());
             return;
         }
         
+        // Add '(' if any are left
         if(left > 0){
             int len = s.length();
             generation(res, s.append("("), left-1, right);
             s.setLength(len);
         }
-        
+
+        //Add ')' only if it won't unbalance the string
         if(left < right){
             int len = s.length();
             generation(res, s.append(")"), left, right-1);
