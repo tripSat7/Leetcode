1513cf719ed7eeeae2e5b28f9743f7152f8c978f
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-15 16:24:32 -0400

    Time: 5 ms (99.43%), Space: 44.7 MB (82.92%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,18 +1,36 @@
+// TC : O(n)
+// SC : O(1)
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] arr = new int[26];
-        int maxFreq = 0, beg = 0, maxlen = 0;
-        char[] ans = s.toCharArray();
-        
-        for(int end = 0; end < ans.length; end ++){
-            arr[ans[end] - 'A']++;
-            maxFreq = Math.max(maxFreq, arr[ans[end] - 'A']);
-            while(end - beg + 1 - maxFreq > k){
-                arr[ans[beg] - 'A']--;
-                beg ++;
+        int[] freq = new int[26];         // Frequency of each uppercase character
+        int maxFreq = 0;                  // Max frequency of any character in current window
+        int start = 0, res = 0;
+        char[] ch = s.toCharArray();
+
+        for (int end = 0; end < ch.length; end++) {
+            freq[ch[end] - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[ch[end] - 'A']);
+
+            // Shrink window if replacements needed exceed k
+             while( (end+1) - start - maxFreq > k){
+                freq[ch[start] - 'A']--;
+                start++;
             }
+
+            // Update the max length of valid window
+            res = Math.max(res, ((end + 1) - start) );
         }
-        
-        return s.length() - beg;
+
+        return res;
     }
 }
+
+/* AABABBA, k = 1
+    start       End     MaxFreq     res 
+    0           0       1           1
+    0           1       2           2
+    0           2       2           3
+    0           3       3           4
+    0           4       3           4
+    1           5       3           4
+*/
