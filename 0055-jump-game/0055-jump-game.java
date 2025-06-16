b7021fb406bf9c3643e874f8f8393aa23d08fda8
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-16 09:33:49 -0400

    Time: 64 ms (14.58%), Space: 46 MB (32.19%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,4 +1,5 @@
-// TC: O(n^2), SC: O(n)
+// TC: O(n^2)
+// SC: O(n)
 
 enum Index {
     GOOD,
@@ -7,35 +8,24 @@ enum Index {
 }
 
 public class Solution {
-
-    Index[] memo; // memo[i] stores if index i is GOOD/BAD/UNKNOWN
-
     public boolean canJump(int[] nums) {
-        memo = new Index[nums.length];
+        Index[] memo = new Index[nums.length];
+
         for (int i = 0; i < memo.length; i++) {
             memo[i] = Index.UNKNOWN;
         }
-        memo[memo.length - 1] = Index.GOOD; // last index is always GOOD
+        memo[memo.length - 1] = Index.GOOD;
 
-        return canJumpFromPosition(0, nums); // start from index 0
-    }
-
-    public boolean canJumpFromPosition(int position, int[] nums) {
-        // Return result if already computed
-        if (memo[position] != Index.UNKNOWN) {
-            return memo[position] == Index.GOOD;
-        }
-
-        // Explore all next reachable positions
-        int furthestJump = Math.min(position + nums[position], nums.length - 1);
-        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-            if (canJumpFromPosition(nextPosition, nums)) {
-                memo[position] = Index.GOOD; // memoize and return
-                return true;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
             }
         }
 
-        memo[position] = Index.BAD; // no valid path from here
-        return false;
+        return memo[0] == Index.GOOD;
     }
 }
