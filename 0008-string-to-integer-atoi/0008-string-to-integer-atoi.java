97f9b74931a9ee20a959aa1eac4448779c327a0a
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-18 16:38:58 -0400

    Time: 1 ms (100%), Space: 42.3 MB (91.22%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,32 +1,46 @@
+// TC : O(N)
+// SC : O(1)
 class Solution {
-    public int myAtoi(String s) {
-        int res=0;
-        int i=0;
-        int flag=1;
-        int max=Integer.MAX_VALUE;
-        int min=Integer.MIN_VALUE;
-        
-        if(s.length()==0){
-            return 0;
-        }
-        while(i<s.length() && s.charAt(i)==' '){
-            i++;
+    public int myAtoi(String input) {
+        int sign = 1;
+        int res = 0;
+        int index = 0;
+        int n = input.length();
+
+        // Discard all spaces from the beginning of the input string.
+        while (index < n && input.charAt(index) == ' ') {
+            index++;
         }
 
-        if(i<s.length() && (s.charAt(i)=='-'|| s.charAt(i)=='+')){
-            if(s.charAt(i)=='-'){
-                flag=-1;
-            }
-            i++;
+        // sign = +1, if it's positive number, otherwise sign = -1.
+        if (index < n && input.charAt(index) == '+') {
+            sign = 1;
+            index++;
+        } 
+        else if (index < n && input.charAt(index) == '-') {
+            sign = -1;
+            index++;
         }
-        while(i<s.length() && s.charAt(i)-'0'<=9 && s.charAt(i)-'0'>=0){
-            int digit = s.charAt(i)-'0';
-            if(res>max/10 || (res==max/10 && digit>max%10)){
-                return (flag == 1) ? max : min;            
+
+        // Traverse next digits of input and stop if it is not a digit
+        while (index < n && Character.isDigit(input.charAt(index))) {
+            int digit = input.charAt(index) - '0';
+
+            // Check overflow(res * 10 case in line 36) and underflow conditions.
+            if ((res > Integer.MAX_VALUE / 10) ||
+                (res == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) 
+            {
+                // If integer overflowed return 2^31-1, otherwise if underflowed return -2^31.
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            res=res*10+digit;
-            i++;
+
+            // Append current digit to the result.
+            res = 10 * res + digit;
+            index++;
         }
-        return res*flag;
+
+        // We have formed a valid number without any overflow/underflow.
+        // Return it after multiplying it with its sign.
+        return sign * res;
     }
 }
