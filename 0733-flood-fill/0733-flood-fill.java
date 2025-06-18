38f301c7318cf17599984328def7fdc5db7d72e8
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-18 13:38:58 -0400

    Time: 1 ms (48.58%), Space: 44.9 MB (83.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,37 @@
-// Time Complexity: O(N * M), worst case the entire grid needs to be traversed
-// Space Complexity: O(N)
+// Time Complexity: O(N * M), where N = rows, M = cols
+// Space Complexity: O(N * M) for the queue in the worst case
+
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
-        if(image[sr][sc] == color){ 
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int originalColor = image[sr][sc];
+        // No work if the color is already the target
+        if (originalColor == newColor){
             return image;
+        } 
+
+        int rows = image.length, cols = image[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr, sc});
+        image[sr][sc] = newColor;
+
+        // Directions: up, down, left, right
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            int r = cell[0], c = cell[1];
+
+            // Visit all 4 neighbors
+            for (int[] d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                // If in-bounds and matches the original color, fill and enqueue
+                if ((nr >= 0 && nr < rows) && (nc >= 0 && nc < cols) && image[nr][nc] == originalColor) {
+                    image[nr][nc] = newColor;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
         }
-        
-        dfs(image, sr, sc, image[sr][sc], color);
-        
+
         return image;
-    }
-    
-    public void dfs(int[][] image, int sr, int sc, int curr, int color){
-        
-        if( sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length ||
-            image[sr][sc] != curr){
-                return;
-        }
-        
-        image[sr][sc] = color;
-        dfs(image, sr-1, sc, curr, color);
-        dfs(image, sr+1, sc, curr, color);
-        dfs(image, sr, sc+1, curr, color);
-        dfs(image, sr, sc-1, curr, color);
-        
     }
 }
