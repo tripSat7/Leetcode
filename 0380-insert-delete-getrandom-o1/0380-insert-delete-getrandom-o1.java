4e3c303d5a07fbce4b3f7f239e7c3b53fe423ac7
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-18 12:13:32 -0400

    Time: 26 ms (92.04%), Space: 92.4 MB (25.22%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,6 +1,10 @@
+// TC : O(1)
+// SC : O(n)
+// explanation - check editorial
+
 public class RandomizedSet {
-    private Map<Integer, Integer> numMap;
-    private List<Integer> nums;
+    private Map<Integer, Integer> numMap; // value -> its index in 'nums' list
+    private List<Integer> nums;           // list of values for O(1) random access
     private Random rand;
 
     public RandomizedSet() {
@@ -10,29 +14,37 @@ public class RandomizedSet {
     }
 
     public boolean insert(int val) {
-        if (numMap.containsKey(val)){
+        // If already present, no insertion
+        if (numMap.containsKey(val)) {
             return false;
-        } 
+        }
+        // Append to list and record its index
         numMap.put(val, nums.size());
         nums.add(val);
         return true;
     }
 
     public boolean remove(int val) {
-        if (!numMap.containsKey(val)){
+        // If not present, can't remove
+        if (!numMap.containsKey(val)) {
             return false;
-        } 
+        }
+
+        // Get index of element to remove
         int idx = numMap.get(val);
-        int last = nums.get(nums.size() - 1);
+        // Move last element into 'idx' to fill the gap
+        int last = nums.get(nums.size() - 1); 
         nums.set(idx, last);
-        numMap.put(last, idx);
+        numMap.put(last, idx);    // Update moved element's index
+        // Remove last element
         nums.remove(nums.size() - 1);
-        numMap.remove(val);
+        numMap.remove(val); // Remove mapping for deleted value
         
         return true;
     }
 
     public int getRandom() {
+        // Pick a random index in [0, nums.size())
         return nums.get(rand.nextInt(nums.size()));
     }
 }
