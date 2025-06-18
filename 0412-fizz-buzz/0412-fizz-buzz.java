aaffa5d6e623f725085c772db5d73ce5921c8e0d
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-18 12:26:17 -0400

    Time: 3 ms (5.69%), Space: 45.8 MB (5.39%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,7 +3,6 @@
 
 class Solution {
     public List<String> fizzBuzz(int n) {
-
         // ans list
         List<String> ans = new ArrayList<String>();
 
@@ -12,19 +11,25 @@ class Solution {
             boolean divisibleBy3 = (num % 3 == 0);
             boolean divisibleBy5 = (num % 5 == 0);
 
-            if (divisibleBy3 && divisibleBy5) {
-                // Divides by both 3 and 5, add FizzBuzz
-                ans.add("FizzBuzz");
-            } else if (divisibleBy3) {
+            String numAnsStr = "";
+
+            if (divisibleBy3) {
                 // Divides by 3, add Fizz
-                ans.add("Fizz");
-            } else if (divisibleBy5) {
-                // Divides by 5, add Buzz
-                ans.add("Buzz");
-            } else {
-                // Not divisible by 3 or 5, add the number
-                ans.add(Integer.toString(num));
+                numAnsStr += "Fizz";
             }
+
+            if (divisibleBy5) {
+                // Divides by 5, add Buzz
+                numAnsStr += "Buzz";
+            }
+
+            if (numAnsStr.equals("")) {
+                // Not divisible by 3 or 5, add the number
+                numAnsStr += Integer.toString(num);
+            }
+
+            // Append the current answer str to the ans list
+            ans.add(numAnsStr);
         }
 
         return ans;
