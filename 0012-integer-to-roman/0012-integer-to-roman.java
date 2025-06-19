4614e5e738e150b935938bfd224985d7250213e7
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-19 16:41:22 -0400

    Time: 3 ms (98.46%), Space: 44.3 MB (71.78%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,20 +1,35 @@
+//TC: O(1) - logic executes a small, bounded number of steps 
+//SC: O(1)
+
 class Solution {
+    static String[] symbol = new String[]{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    static int[] value = new int[]{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    
     public String intToRoman(int num) {
-        String[] ones = new String[]{"","I","II","III","IV","V", "VI","VII","VIII","IX"};
-        String[] tens = new String[]{"","X", "XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String[] hundreds = new String[]{"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String[] thousands = new String[]{"","M","MM","MMM"};
 
-       StringBuilder sb = new StringBuilder();
-       String thousand = thousands[num/1000];
-       String hundred = hundreds[(num%1000)/100];
-       String ten = tens[(num%100)/10];
-       String one = ones[num%10];
-        sb.append(thousand);
-        sb.append(hundred);
-        sb.append(ten);
-        sb.append(one);
-
+        
+        StringBuilder sb = new StringBuilder();
+        int i = value.length - 1;
+        
+        //Logic
+        //Subtract num with value[i] till num is greater than Zero, 
+        // if num is greater/equal to value[i], and append Corresponding symbol
+        // else just move lower value
+         
+        while(num > 0 && i >= 0){
+            
+            if(num < value[i]){
+                //just move to lower value
+                i--;
+            } else {
+                //decreament num with Current value
+                num -= value[i];
+                
+                //now append the corresponding symbol
+                sb.append(symbol[i]);
+            }
+        }
+        
         return sb.toString();
     }
 }
