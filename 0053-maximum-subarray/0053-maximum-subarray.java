8ef33b2a1fc5af9939993148e36ef4a765556fcd
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-20 02:47:25 -0400

    Time: 1 ms (99.6%), Space: 57.1 MB (56.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,20 @@
+// TC : O(n)
+// SC : O(1)
+
 class Solution {
     public int maxSubArray(int[] nums) {
-        int res = nums[0];
-        int maxEnding = nums[0];
-        
-        for(int i = 1; i < nums.length; i++){
-            maxEnding = Math.max(maxEnding + nums[i], nums[i]);
-            res = Math.max(maxEnding, res);
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0]; //will keep the running count of the current subarray
+        int maxSubarray = nums[0]; //will always have bigger subarray
+
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
         }
-        
-        return res;
+
+        return maxSubarray;
     }
 }
