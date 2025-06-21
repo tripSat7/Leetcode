a0814401462d8c254a7a1de949a919ef7da34c8a
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-21 05:36:22 -0400

    Time: 3 ms (99.1%), Space: 60.1 MB (42.95%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,16 +1,32 @@
+// Time complexity: O(n+m)
+// Space complexity: O(m)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int num : nums){
-            pq.offer(num);
-            
-            if(pq.size()>k){
-                pq.poll();
-            }
-            
+        // Find minimum and maximum values in the array
+        int minValue = Integer.MAX_VALUE;
+        int maxValue = Integer.MIN_VALUE;
+        for (int num : nums) {
+            minValue = Math.min(minValue, num);
+            maxValue = Math.max(maxValue, num);
         }
         
-        return pq.poll();
+        // Create and populate the counting array for all possible values
+        int[] count = new int[maxValue - minValue + 1];
+        for (int num : nums) {
+            count[num - minValue]++;
+        }
+        
+        // Iterate from the largest value towards the smallest
+        int remain = k; // Number of elements left to skip to reach the k-th largest
+        for (int num = count.length - 1; num >= 0; num--) {
+            remain -= count[num];
+            // When we've skipped k elements, return the current number
+            if (remain <= 0) {
+                return num + minValue;
+            }
+        }
+        
+        // This should not be reached for valid input
+        return -1;
     }
 }
