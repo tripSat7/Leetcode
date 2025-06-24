506c221ac6adc4db82d1c7828c38d418c2f9454b
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-24 03:23:44 -0400

    Time: 5 ms (75.23%), Space: 58 MB (42.87%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,20 +1,31 @@
-class Solution {
+// Time Complexity: O(N) 
+// Space Complexity: O(1)
+
+public class Solution {
     public int maxArea(int[] height) {
-        int max = 0;
-        int i=0, j=height.length-1;
-        while(i<j){
-            int len = j-i;
-            int area = height[i]>height[j]?height[j]*len:height[i]*len;
-            if(area>max){
-                max = area;
-            }
-            if(height[i]>height[j]){
-                j--;
-            }else{
-                i++;
+        int maxarea = 0;
+        int left = 0;
+        int right = height.length - 1;  
+
+        
+        while (left < right) {
+            int width = right - left;    // Width between the two lines
+
+            // Area is the shorter of the two heights times the width
+            maxarea = Math.max(
+                maxarea,
+                Math.min(height[left], height[right]) * width
+            );
+
+            // Move the pointer at the shorter line inward,
+            // since moving the taller one cannot increase area
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
-        
-        return max;
+
+        return maxarea;
     }
 }
