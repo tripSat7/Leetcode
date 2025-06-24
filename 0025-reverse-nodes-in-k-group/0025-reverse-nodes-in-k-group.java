051a8594a7023d25cc3078456728ea7fef718e2f
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-24 15:28:55 -0400

    Time: 0 ms (100%), Space: 44.7 MB (14.11%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -1,3 +1,6 @@
+// Time Complexity: O(N) - Each node is visited a constant number of times (counting, splitting, and reversing).
+// Space Complexity: O(1)
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -12,55 +15,59 @@ class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         int count = 0;
         ListNode temp = head;
-        while(temp!=null){
+        // Count total number of nodes
+        while (temp != null) {
             temp = temp.next;
             count++;
         }
-        ListNode prevTail = null;
-        ListNode curHead = head;
-        ListNode curTail = head;
-        ListNode newHead = null;
-        count = count/k;
-        
-        while(count > 0){
-            int tmp = k-1;
-            while(tmp > 0){
+
+        ListNode prevTail = null;    // End of the previous reversed block
+        ListNode curHead = head;     // Start of the current block
+        ListNode curTail = head;     // End of the current block
+        ListNode newHead = null;     // Next node after current block
+        count = count / k;           // Number of full k-groups to reverse
+
+        // Process each k-group
+        while (count > 0) {
+            int tmp = k - 1;
+            // Move curTail to end of current k-group
+            while (tmp > 0) {
                 curTail = curTail.next;
                 tmp--;
             }
-            
-            newHead = curTail.next;
-            curTail.next = null;
-            curTail = reverse(curHead);
-            if(prevTail == null){
+
+            newHead = curTail.next;    // Save next group start
+            curTail.next = null;       // Cut the current group
+            curTail = reverse(curHead);// Reverse the current group
+
+            if (prevTail == null) {
+                // For first block, update head
                 prevTail = curHead;
                 head = curTail;
-            }
-            else{
+            } else {
+                // Connect previous block to current reversed head
                 prevTail.next = curTail;
                 prevTail = curHead;
             }
-            curHead.next = newHead;
-            curHead = newHead;
+
+            curHead.next = newHead;    // Connect reversed group to the next block
+            curHead = newHead;         // Move to the next group
             curTail = newHead;
             count--;
         }
-        
+
         return head;
     }
-    
-    public ListNode reverse(ListNode head){
-        ListNode curr = head;
-        ListNode prev = null;
-        ListNode nxt = head;
-        
-        while(nxt!=null){
+
+    // Reverse the given linked list and return new head
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head, prev = null, nxt = head;
+        while (nxt != null) {
             nxt = curr.next;
             curr.next = prev;
             prev = curr;
             curr = nxt;
         }
-        
         return prev;
     }
 }
