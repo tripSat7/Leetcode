e323d6b30aae143ec49bc6aef03ed31f25a9cf81
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-24 04:14:24 -0400

    Time: 24 ms (98.13%), Space: 51.3 MB (92.26%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,42 @@
-class Solution {
+// Time Complexity: O(N^2)
+//   - Outer loop: O(N)
+//   - Inner two-pointer scan: O(N) for each i
+// Space Complexity: O(N) (for sorting, if not in-place; otherwise, O(1) extra)
+
+public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums); 
+        List<List<Integer>> res = new ArrayList<>();
 
-        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0)
+            {
+                break; // Since array is sorted, no need to check further
+            } 
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            if(nums[i] > 0){
-                break;
-            }
-            int j = i + 1;
-            int k = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue; // Skip duplicate values for i
+            } 
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } 
+                else if (sum < 0) {
+                    l++;
+                } 
+                else {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    // Skip duplicates for both pointers
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
