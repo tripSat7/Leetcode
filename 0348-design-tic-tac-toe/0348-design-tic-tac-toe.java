483014a03fe0dee1eb8526b574d92db37b5be093
Saatvik Tripathy <117186874+tripSat7@users.noreply.github.com>
2025-06-25 01:06:14 -0400

    Time: 3 ms (99.88%), Space: 45.5 MB (72.35%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
@@ -1,67 +1,49 @@
-// TC : O(n)
-// SC : O(n^2)  
+// TC : O(1)
+// SC : O(n)  
 
-class TicTacToe {
-
-    private int[][] board;
-    private int n;
+// check editorial slideshow for approach
+public class TicTacToe {
+    int[] rows;
+    int[] cols;
+    int diagonal;
+    int antiDiagonal;
 
     public TicTacToe(int n) {
-        board = new int[n][n];
-        this.n = n;
+        rows = new int[n];
+        cols = new int[n];
     }
 
     public int move(int row, int col, int player) {
-        board[row][col] = player;
+        int currentPlayer = (player == 1) ? 1 : -1;
 
-        // check if the player wins
-        if ((checkRow(row, player)) ||
-            (checkColumn(col, player)) ||
-            (row == col && checkDiagonal(player)) ||
-            (col == n - row - 1 && checkAntiDiagonal(player))) {
+        // update currentPlayer in rows and cols arrays
+        rows[row] += currentPlayer;
+        cols[col] += currentPlayer;
+        
+        // update diagonal
+        if (row == col) {
+            diagonal += currentPlayer;
+        }
+        
+        //update anti diagonal
+        if (col == (cols.length - row - 1)) {
+            antiDiagonal += currentPlayer;
+        }
+        int n = rows.length;
+        
+        // check if the current player wins
+        if (Math.abs(rows[row]) == n ||
+                Math.abs(cols[col]) == n ||
+                Math.abs(diagonal) == n ||
+                Math.abs(antiDiagonal) == n) {
             return player;
         }
-
+        
         // No one wins
         return 0;
     }
-
-    private boolean checkDiagonal(int player) {
-        for (int row = 0; row < n; row++) {
-            if (board[row][row] != player) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkAntiDiagonal(int player) {
-        for (int row = 0; row < n; row++) {
-            if (board[row][n - row - 1] != player) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkColumn(int col, int player) {
-        for (int row = 0; row < n; row++) {
-            if (board[row][col] != player) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkRow(int row, int player) {
-        for (int col = 0; col < n; col++) {
-            if (board[row][col] != player) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
+
 
 /**
  * Your TicTacToe object will be instantiated and called as such:
